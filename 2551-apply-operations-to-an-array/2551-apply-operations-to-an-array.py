ebce4c74bc526aa94e7ad65c8e35045a544d4c0e
Nick Weseman <trumpernickel2@yahoo.com>
2025-05-27 16:01:03 -0500

    Time: 0 ms (100%), Space: 18.1 MB (7.47%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -5,13 +5,12 @@ class Solution:
                 nums[i] *= 2
                 nums[i + 1] = 0
         
-        slow_idx = 0
+        slow = 0
 
-        for fast_idx in range(1, len(nums)):
-            if nums[slow_idx] == 0 and nums[fast_idx] != 0:
-                nums[slow_idx], nums[fast_idx] = nums[fast_idx], nums[slow_idx]
-            if nums[slow_idx] != 0:
-                slow_idx += 1
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1 
         return nums
 
         
