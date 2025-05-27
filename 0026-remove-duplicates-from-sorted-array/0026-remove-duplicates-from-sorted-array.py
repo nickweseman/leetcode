28b31dc9e31dd7226b6247c6542744019a37f526
Nick Weseman <trumpernickel2@yahoo.com>
2025-05-27 12:17:36 -0500

    Time: 4 ms (26.44%), Space: 18.9 MB (74.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         slowIndex = 1
 
         for fastIndex in range(1, len(nums)):
-            if nums[slowIndex - 1] < nums[fastIndex]:
+            if nums[slowIndex - 1] != nums[fastIndex]:
                 nums[slowIndex] = nums[fastIndex]
                 slowIndex += 1
         return slowIndex
