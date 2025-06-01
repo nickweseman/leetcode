a464b725719ab5b063d86ff0391067dceb2e2160
Nick Weseman <trumpernickel2@yahoo.com>
2025-05-31 19:43:33 -0500

    Time: 0 ms (100%), Space: 17.7 MB (66.94%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        need = scan = 0
+
+        while need < len(s) and scan < len(t):
+            if s[need] == t[scan]:
+                need += 1
+            scan += 1
+        return need == len(s)
+        
