a3a274415ae4cdc973ace3633728300cc02e3117
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-02 17:03:44 -0500

    Time: 0 ms (100%), Space: 18.2 MB (16.4%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -5,21 +5,20 @@ class Solution:
 
         while i < len(nums1) and j < len(nums2):
             if nums1[i][0] < nums2[j][0]:
-                output.append([nums1[i][0], nums1[i][1]])
+                output.append(nums1[i])
                 i += 1
             elif nums1[i][0] > nums2[j][0]:
-                output.append([nums2[j][0], nums2[j][1]])
+                output.append(nums2[j])
                 j += 1
             else:
                 output.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
-                i+= 1
-                j+= 1
+                i += 1
+                j += 1
         while i < len(nums1):
-            output.append([nums1[i][0], nums1[i][1]])
+            output.append(nums1[i])
             i += 1
         while j < len(nums2):
-            output.append([nums2[j][0], nums2[j][1]])
+            output.append(nums2[j])
             j += 1
         return output
-
         
