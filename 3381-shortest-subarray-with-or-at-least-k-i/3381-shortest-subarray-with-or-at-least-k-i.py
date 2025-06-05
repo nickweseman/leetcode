f1ae1b3d09d5c435ccd5b9969694aef6e5d5f9ec
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-05 14:40:25 -0500

    Time: 28 ms (12.9%), Space: 17.9 MB (42.74%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
@@ -1,41 +1,34 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
         left = right = 0
-        right = 0
-        
-        # Technically only need 6 bits since nums[i] <= 50 (50 in binary = 110010 (6 bits)) and k < 64 (63 = 111111 (6 bits))
-        bit_count = [0] * 32
-        
+        NUM_BITS = 32
+        bits = [0] * 32
         min_length = float('inf')
+
+        def add(num: int) -> None:
+            for i in range(NUM_BITS):
+                if (num >> i) & 1:
+                    bits[i] += 1
         
-        def add(num):
-            for i in range(6):
+        def remove(num: int) -> None:
+            for i in range(NUM_BITS):
                 if (num >> i) & 1:
-                    bit_count[i] += 1
-
-        def remove(num):
-            for i in range(6):
-                if (num >> i) & 1:
-                    bit_count[i] -= 1
-
-        def get_current_or():
+                    bits[i] -= 1
+        
+        def total_or(num: int) -> bool:
             result = 0
-            for i in range(6):
-                if bit_count[i] > 0:
-                    result |= (1 << i) 
+            for i in range(NUM_BITS):
+                if bits[i] >= 1:
+                    result |= 1 << i
             return result
         
         while right < len(nums):
             add(nums[right])
-            
-            # Shrink while window is still valid (OR >= k)
-            while left <= right and get_current_or() >= k:
-                # Update answer BEFORE shrinking
+
+            while left <= right and total_or(nums[left]) >= k:
                 min_length = min(min_length, right - left + 1)
-                
+
                 remove(nums[left])
                 left += 1
             right += 1
-        
         return min_length if min_length != float('inf') else -1
-        
