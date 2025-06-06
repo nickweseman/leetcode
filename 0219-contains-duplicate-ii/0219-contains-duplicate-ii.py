f36bed9ba05589e08d3a0ad9eabeb8b4847cb081
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-06 11:52:34 -0500

    Time: 84 ms (7.94%), Space: 33.4 MB (68%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,24 +1,17 @@
 from collections import defaultdict
-
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        left = right = 0
+        left, right = 0, 0
         window = defaultdict(int)
 
         while right < len(nums):
-            right_element = nums[right]
-            window[right_element] += 1
+            window[nums[right]] += 1
 
-            while right - left > k:
-                left_element = nums[left]
-                window[left_element] -= 1
-
-                if window[left_element] == 0:
-                    del window[left_element]
-                
+            while (right - left) > k:
+                window[nums[left]] -= 1
                 left += 1
             
-            if window[right_element] > 1:
+            if window[nums[right]] > 1:
                 return True
             right += 1
         return False
