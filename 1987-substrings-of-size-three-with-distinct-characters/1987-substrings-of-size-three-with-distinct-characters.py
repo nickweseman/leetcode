839b1f5bd146b857375c218e643be0509eb5be3a
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-06 13:34:43 -0500

    Time: 3 ms (39.88%), Space: 17.9 MB (8.57%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
@@ -1,21 +1,25 @@
 from collections import defaultdict
-
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
         left = right = 0
         window = defaultdict(int)
         substrings = 0
 
+        def is_valid() -> bool:
+            nonlocal window
+            for freq in window.values():
+                if freq > 1:
+                    return False
+            return True
+
         while right < len(s):
             window[s[right]] += 1
 
-            while window[s[right]] > 1 or right - left + 1 > 3:
-               window[s[left]] -= 1
-               left += 1
-
-            if right - left + 1 == 3:
+            if right - left + 1 > 3:
+                window[s[left]] -= 1
+                left += 1
+            
+            if right - left + 1 == 3 and is_valid():
                 substrings += 1
-
             right += 1
         return substrings
-        
