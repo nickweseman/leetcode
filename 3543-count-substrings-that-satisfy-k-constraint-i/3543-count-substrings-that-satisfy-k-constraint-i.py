47bd2b0783ff7a26f298c6c084eb29b3a0dc6c3b
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-06 13:29:05 -0500

    Time: 3 ms (83.88%), Space: 17.8 MB (58.35%) - LeetHub

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        left = right = 0
+        window = defaultdict(int)
+        substrings = 0
+
+        while right < len(s):
+            window[s[right]] += 1
+
+            while window['0'] > k and window['1'] > k:
+                window[s[left]] -= 1
+                left += 1
+            substrings += right - left + 1
+            right += 1
+        return substrings
+        
