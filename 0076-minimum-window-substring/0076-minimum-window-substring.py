7df5995c41d417f50ffc24676feec01bb5ebab12
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-07 09:02:35 -0500

    Time: 103 ms (42.22%), Space: 18.5 MB (16.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,5 +1,4 @@
-from collections import Counter, defaultdict
-
+from collections import defaultdict, Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         target = Counter(t)
@@ -8,35 +7,33 @@ class Solution:
         have = 0
         need = len(target)
 
-        left, right = 0, 0
+        left = right = 0
         best_left, best_right = 0, float('inf')
 
         def add(c: str) -> None:
             nonlocal have
             window[c] += 1
-            if window[c] == target[c]:
+            if target[c] == window[c]:
                 have += 1
         
         def remove(c: str) -> None:
             nonlocal have
-            if window[c] == target[c]:
+            if target[c] == window[c]:
                 have -= 1
             window[c] -= 1
-
-        def window_valid() -> bool:
-            nonlocal have, need
-            return have == need
         
-        def update_answer(l: int, r: int) -> None:
+        def window_valid() -> bool:
+            return left <= right and have == need
+        
+        def update_answer() -> None:
             nonlocal best_left, best_right
-            if r - l < best_right - best_left:
-                best_left, best_right = l, r
+            if right - left < best_right - best_left:
+                best_left, best_right = left, right
         
         while right < len(s):
             add(s[right])
-
-            while left <= right and window_valid():
-                update_answer(left, right)
+            while window_valid():
+                update_answer()
                 remove(s[left])
                 left += 1
             right += 1
