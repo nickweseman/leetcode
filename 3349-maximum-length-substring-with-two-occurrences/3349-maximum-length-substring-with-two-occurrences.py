5183b1f1fd63216ef9806579c8d01aff3c94c3cb
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-07 07:20:21 -0500

    Time: 2 ms (93.04%), Space: 17.6 MB (97.83%) - LeetHub

--- 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
@@ -8,12 +8,9 @@ class Solution:
         while right < len(s):
             window[s[right]] += 1
 
-            while left < right and window[s[right]] > 2:
+            while window[s[right]] > 2:
                 window[s[left]] -= 1
                 left += 1
-            
             max_length = max(max_length, right - left + 1)
             right += 1
         return max_length
-
-        
