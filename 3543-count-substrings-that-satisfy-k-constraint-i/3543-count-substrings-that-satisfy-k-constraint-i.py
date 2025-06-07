2705717e8adc2728b07cca495f964339dc398de5
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-07 08:09:04 -0500

    Time: 10 ms (55.13%), Space: 18 MB (16.63%) - LeetHub

--- 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.py
@@ -1,17 +1,23 @@
 from collections import defaultdict
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
-        left = right = 0
         window = defaultdict(int)
         substrings = 0
+        left = right = 0
+
+        def window_invalid() -> None:
+            return window['0'] > k and window['1'] > k
+
+        def update_answer() -> None:
+            nonlocal substrings
+            substrings += right - left + 1
 
         while right < len(s):
             window[s[right]] += 1
 
-            while window['0'] > k and window['1'] > k:
+            while window_invalid():
                 window[s[left]] -= 1
                 left += 1
-            substrings += right - left + 1
+            update_answer()
             right += 1
         return substrings
-        
