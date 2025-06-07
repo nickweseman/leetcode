8e588a4a58fe9b79e6d1c71119921166e9f92245
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-07 07:26:58 -0500

    Time: 0 ms (100%), Space: 17.6 MB (92.49%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.py
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.py
@@ -1,15 +1,14 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
-        left = right = 0
         substrings = 0
+        left = right = 0
         s = str(num)
 
         while right < len(s):
             if right - left + 1 > k:
                 left += 1
-            
-            sub = int(s[left:right+1])
-            if right - left + 1 == k and sub != 0 and num % sub == 0:
+            val = int(s[left:right+1])
+            if right - left + 1 == k and val != 0 and num % val == 0:
                 substrings += 1
             right += 1
         return substrings
