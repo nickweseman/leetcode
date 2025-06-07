dedb50d7f6d63f5ffcf55c931f9203040a7ce6a1
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-07 07:36:48 -0500

    Time: 168 ms (5.05%), Space: 27.3 MB (78.1%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,8 +1,8 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         max_avg = float('-inf')
-        window = 0
         left = right = 0
+        window = 0
 
         while right < len(nums):
             window += nums[right]
@@ -10,9 +10,10 @@ class Solution:
             if right - left + 1 > k:
                 window -= nums[left]
                 left += 1
-           
+            
             if right - left + 1 == k:
                 max_avg = max(max_avg, window / (right - left + 1))
             right += 1
         return max_avg
+
         
