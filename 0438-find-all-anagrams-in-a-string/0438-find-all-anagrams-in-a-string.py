d2aebdd8df6595a9fd71113ffd024918d8959b1e
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-07 19:35:10 -0500

    Time: 95 ms (46.13%), Space: 18.4 MB (36.07%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -7,16 +7,23 @@ class Solution:
         left, right = 0, 0
         anagrams = []
 
+        matches = 0
+        required_matches = len(target)
+
         while right < len(s):
             window[s[right]] += 1
+            if window[s[right]] == target[s[right]]:
+                matches += 1
             
             if right - left + 1 > len(p):
+                if window[s[left]] == target[s[left]]:
+                    matches -= 1
                 window[s[left]] -= 1
                 if window[s[left]] == 0:
                     del window[s[left]]
                 left += 1
 
-            if right - left + 1 == len(p) and target == window:
+            if right - left + 1 == len(p) and matches == required_matches:
                 anagrams.append(left)
             right += 1
         return anagrams
