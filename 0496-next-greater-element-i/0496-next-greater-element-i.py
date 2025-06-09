9bd63cf8855d5e13145946ae56202e2d6072aa1e
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-09 18:02:22 -0500

    Time: 0 ms (100%), Space: 18.2 MB (20.93%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,26 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        """
-        Brute force: For each element in nums1, 
-        find it in nums2, then look for next greater
-        """
-        result = []
+        next_greater = {}
+        stack = []
         
-        for num in nums1:
-            # First, find where num is in nums2
-            found_at = -1
-            for i in range(len(nums2)):
-                if nums2[i] == num:
-                    found_at = i
-                    break
-            
-            # Then, look for next greater element after that position
-            next_greater = -1
-            for j in range(found_at + 1, len(nums2)):
-                if nums2[j] > num:
-                    next_greater = nums2[j]
-                    break
-            
-            result.append(next_greater)
+        for num in nums2:
+            while stack and stack[-1] < num:
+                next_greater[stack.pop()] = num
+            stack.append(num)
         
-        return result
+        return [next_greater.get(num, -1) for num in nums1]
