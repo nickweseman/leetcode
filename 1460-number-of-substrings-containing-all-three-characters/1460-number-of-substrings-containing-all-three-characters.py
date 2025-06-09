d8d16ad71e14658b7e55812f4e2df4cb2b27de18
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-09 07:19:50 -0500

    Time: 107 ms (59.93%), Space: 18 MB (38.08%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,24 +1,20 @@
-from collections import defaultdict
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        def substringsAtMostK(k: int) -> int:
-            window = defaultdict(int)
-            left = right = 0
-            subarrays = 0
-            distinct = 0
-
-            while right < len(s):
-                window[s[right]] += 1
-                if window[s[right]] == 1:
-                    distinct += 1
-
-                while left <= right and distinct > k:
-                    if window[s[left]] == 1:
-                        distinct -= 1
-                    window[s[left]] -= 1
-                    left += 1
-                subarrays += right - left + 1
-                right += 1
-            return subarrays
-        return substringsAtMostK(3) - substringsAtMostK(2)
+        left = right = 0
+        window = {'a': 0, 'b': 0, 'c': 0}
+        substrings = 0
+        
+        while right < len(s):
+            window[s[right]] += 1
+            
+            while window['a'] > 0 and window['b'] > 0 and window['c'] > 0:
+                # Update answer BEFORE shrinking (current window is valid)
+                # Key insight: If [left...right] contains all 3 chars,
+                # then ALL substrings [0...left], [1...left], ..., [left...left]
+                # extended to right are also valid
+                window[s[left]] -= 1
+                left += 1
+            substrings += left
+            right += 1
+        return substrings
         
