421c482223250ba15985a420ca6e52227850989a
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-10 06:29:15 -0500

    Time: 0 ms (100%), Space: 17.9 MB (65.24%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -4,7 +4,8 @@ class Solution:
         stack = []
 
         for num in nums2:
-            while stack and num > stack[-1]:
-                next_greater[stack.pop()] = num
+            while stack and stack[-1] < num:
+                next_greater[stack.pop()] = num 
             stack.append(num)
         return [next_greater.get(num, -1) for num in nums1]
+        
