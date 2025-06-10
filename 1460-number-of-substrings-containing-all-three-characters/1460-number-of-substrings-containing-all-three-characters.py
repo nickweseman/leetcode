acca5a7a081a5d3b840e1951f75d17d343fb2016
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-10 12:39:05 -0500

    Time: 116 ms (41.08%), Space: 17.8 MB (64.04%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,20 +1,21 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        left = right = 0
-        window = {'a': 0, 'b': 0, 'c': 0}
-        substrings = 0
-        
-        while right < len(s):
-            window[s[right]] += 1
-            
-            while window['a'] > 0 and window['b'] > 0 and window['c'] > 0:
-                # Update answer BEFORE shrinking (current window is valid)
-                # Key insight: If [left...right] contains all 3 chars,
-                # then ALL substrings [0...left], [1...left], ..., [left...left]
-                # extended to right are also valid
-                window[s[left]] -= 1
-                left += 1
-            substrings += left
-            right += 1
-        return substrings
+        n = len(s)
+        total_substrings = (n * (n + 1)) // 2
+
+        def atMost2() -> int:
+            window = {'a': 0, 'b': 0, 'c': 0}
+            left = right = 0
+            substrings = 0
+
+            while right < len(s):
+                window[s[right]] += 1
+
+                while window['a'] and window['b'] and window['c']:
+                    window[s[left]] -= 1
+                    left += 1
+                substrings += right - left + 1
+                right += 1
+            return substrings
+        return total_substrings - atMost2()
         
