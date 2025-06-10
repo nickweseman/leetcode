2987a33a5b8ed0a1f2f5b65b9d1abd7bfbc6d7da
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-10 06:39:06 -0500

    Time: 102 ms (42.51%), Space: 18.2 MB (76.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,41 +1,30 @@
-from collections import defaultdict, Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        target = Counter(t)
-        window = defaultdict(int)
-
-        have = 0
-        need = len(target)
+        window = collections.Counter()
+        target = collections.Counter(t)
 
         left = right = 0
         best_left, best_right = 0, float('inf')
 
-        def add(c: str) -> None:
-            nonlocal have
-            window[c] += 1
-            if target[c] == window[c]:
-                have += 1
-        
-        def remove(c: str) -> None:
-            nonlocal have
-            if target[c] == window[c]:
-                have -= 1
-            window[c] -= 1
-        
-        def window_valid() -> bool:
-            return left <= right and have == need
-        
-        def update_answer() -> None:
+        have = 0
+        need = len(target)
+
+        def updateAnswer(l: int, r: int) -> None:
             nonlocal best_left, best_right
-            if right - left < best_right - best_left:
-                best_left, best_right = left, right
+            if r - l < best_right - best_left:
+                best_left, best_right = l, r
         
         while right < len(s):
-            add(s[right])
-            while window_valid():
-                update_answer()
-                remove(s[left])
+            window[s[right]] += 1
+            if window[s[right]] == target[s[right]]:
+                have += 1
+            
+            while left <= right and have == need:
+                updateAnswer(left, right)
+                if window[s[left]] == target[s[left]]:
+                    have -= 1
+                window[s[left]] -= 1
                 left += 1
             right += 1
-        return "" if best_right == float('inf') else s[best_left: best_right + 1]
+        return "" if best_right == float('inf') else s[best_left:best_right + 1]
         
