804cf60c747b5f37c249773da794fee53baf39b8
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-11 06:35:46 -0500

    Time: 124 ms (34.17%), Space: 17.9 MB (63.99%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -4,18 +4,17 @@ class Solution:
         total_substrings = (n * (n + 1)) // 2
 
         def atMost2() -> int:
-            window = {'a': 0, 'b': 0, 'c': 0}
+            window = {'a':0, 'b':0, 'c':0}
             left = right = 0
             substrings = 0
 
             while right < len(s):
                 window[s[right]] += 1
 
-                while window['a'] and window['b'] and window['c']:
+                while left <= right and window['a'] > 0 and window['b'] > 0 and window['c'] > 0:
                     window[s[left]] -= 1
                     left += 1
                 substrings += right - left + 1
                 right += 1
             return substrings
         return total_substrings - atMost2()
-        
