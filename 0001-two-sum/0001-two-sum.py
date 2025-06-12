125edbdb840d912d65c1459c3d425287396ba4f6
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-12 14:38:45 -0500

    Time: 0 ms (100%), Space: 18.8 MB (51.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,11 +3,10 @@ class Solution:
         num_map = {}
 
         for i, num in enumerate(nums):
-            num_map[num] = i
-
-        for i, num in enumerate(nums):
             goal = target - num
+            
             if goal in num_map:
                 i2 = num_map[goal]
                 if i != i2:
                     return [i, i2]
+            num_map[num] = i
