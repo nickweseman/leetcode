14a61398de24175f21e94c663f4503e4ec6752a1
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-12 10:06:04 -0500

    Time: 29 ms (56.31%), Space: 21.3 MB (94.44%) - LeetHub

--- 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.py
+++ 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.py
@@ -7,15 +7,15 @@ class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
         stack = []
         values = []
-
+        
         while head:
             values.append(head.val)
             head = head.next
-        
-        result = [0] * len(values)
 
+        result = [0] * len(values)
+        
         for i, num in enumerate(values):
             while stack and values[stack[-1]] < num:
-                result[stack.pop()] = num 
+                result[stack.pop()] = num
             stack.append(i)
         return result
