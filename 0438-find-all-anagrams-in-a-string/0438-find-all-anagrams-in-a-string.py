4cdc580b2485306b836c6e74092dc067428b1524
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-13 08:05:26 -0500

    Time: 93 ms (45.6%), Space: 18.3 MB (79.26%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -6,15 +6,22 @@ class Solution:
 
         left = right = 0
 
+        have = 0
+        need = len(target)
+
         while right < len(s):
             window[s[right]] += 1
+            if window[s[right]] == target[s[right]]:
+                have += 1
             
             if right - left + 1 > len(p):
+                if window[s[left]] == target[s[left]]:
+                    have -= 1
                 window [s[left]] -= 1
                 if window[s[left]] == 0:
                     del window[s[left]]
                 left += 1
-            if right - left + 1 == len(p) and target == window:
+            if right - left + 1 == len(p) and have == need:
                 result.append(left)
             right += 1
         return result
