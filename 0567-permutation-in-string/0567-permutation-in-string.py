7d56c3635316f415e03a19524525ee04129de129
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-14 07:48:05 -0500

    Time: 82 ms (24.43%), Space: 17.9 MB (39.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,21 +3,25 @@ class Solution:
         window = collections.Counter()
         target = collections.Counter(s1)
         left = right = 0
-
         have = 0
         need = len(target)
 
         while right < len(s2):
             window[s2[right]] += 1
             if window[s2[right]] == target[s2[right]]:
+                #print("HAAAAAAAAAAAAAAVE")
                 have += 1
 
             if right - left + 1 > len(s1):
-                if window[s2[left]] == target[s2[left]]:
+                #print(f"entering if loop what was {right=} {left=}")
+                if window[s2[left]] == 1:
                     have -= 1
                 window[s2[left]] -= 1
+                if window[s2[left]] == 0:
+                    del window[s2[left]]
                 left += 1
-            if right - left + 1 == len(s1) and have == need:
+            #print(locals())
+            if right - left + 1 == len(s1) and window == target:
                 return True
             right += 1
         return False
