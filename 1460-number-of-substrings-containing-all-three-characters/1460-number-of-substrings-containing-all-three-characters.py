c1abf42ea439f4f34c9057a572d1acc8aae8c4da
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-15 12:01:46 -0500

    Time: 115 ms (44.51%), Space: 17.9 MB (64%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,20 +1,16 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        n = len(s)
-        total_substrings = (n * (n + 1)) // 2
+        total_substrings = (len(s) * (len(s) + 1)) // 2
+        substrings = 0
+        left = right = 0
+        window = {"a":0, "b":0, "c":0}
 
-        def atMost2() -> int:
-            window = {'a':0, 'b':0, 'c':0}
-            left = right = 0
-            substrings = 0
+        while right < len(s):
+            window[s[right]] += 1
 
-            while right < len(s):
-                window[s[right]] += 1
-
-                while left <= right and window['a'] > 0 and window['b'] > 0 and window['c'] > 0:
-                    window[s[left]] -= 1
-                    left += 1
-                substrings += right - left + 1
-                right += 1
-            return substrings
-        return total_substrings - atMost2()
+            while window["a"] > 0 and window["b"] > 0 and window["c"] > 0:
+                window[s[left]] -= 1
+                left += 1
+            substrings += right - left + 1
+            right += 1
+        return total_substrings - substrings
