bb2403fa1c1d40eb2fe07ea10c0a4ab8b1dfb091
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-15 10:27:55 -0500

    Time: 120 ms (15.14%), Space: 29.7 MB (14.15%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,21 +1,18 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        total_subarrays = (len(nums) * (len(nums) + 1)) // 2
         maxx = max(nums)
+        total_subarrays = (len(nums) * (len(nums) + 1)) // 2
+        subarrays = 0
+        left = right = 0
+        max_count = 0
 
-        def helper(kk: int) -> int:
-            subarrays = 0
-            left = right = 0
-            max_count = 0
-
-            while right < len(nums):
-                if nums[right] == maxx:
-                    max_count += 1
-                while max_count > kk:
-                    if nums[left] == maxx:
-                        max_count -= 1
-                    left += 1
-                subarrays += right - left + 1
-                right += 1
-            return subarrays
-        return total_subarrays - helper(k - 1)
+        while right < len(nums):
+            if nums[right] == maxx:
+                max_count += 1
+            while max_count >= k:
+                if nums[left] == maxx:
+                    max_count -= 1
+                left += 1
+            subarrays += right - left + 1
+            right += 1
+        return total_subarrays - subarrays
