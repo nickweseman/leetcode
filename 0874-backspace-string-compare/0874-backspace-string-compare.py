0aca48d5d0defeb7c91f947fb0a3749a93371051
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-19 10:17:02 -0500

    Time: 0 ms (100%), Space: 17.7 MB (86.54%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -1,15 +1,12 @@
 class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:     
-        def execute(st: str) -> str:
-            keyboard = []
-            i = 0
-
-            while i < len(st):
-                if st[i] == "#":
-                    if keyboard:
-                        keyboard.pop()
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def execute(ss: str = "") -> str:
+            stack = []
+            for c in ss:
+                if c == "#":
+                    if stack:
+                        stack.pop()
                 else:
-                    keyboard.append(st[i])
-                i += 1
-            return "".join(keyboard)
+                    stack.append(c)
+            return stack
         return execute(s) == execute(t)
