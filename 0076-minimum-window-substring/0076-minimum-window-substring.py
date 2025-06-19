5f0cf3de7b07cae067b00254e2c1d42a888fd824
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-19 11:18:00 -0500

    Time: 143 ms (27.87%), Space: 18.1 MB (77.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -4,27 +4,36 @@ class Solution:
         target = collections.Counter(t)
 
         left = right = 0
-        best_left, best_right = 0, float('inf')
 
         have = 0
         need = len(target)
 
-        def updateAnswer(l: int, r: int) -> None:
-            nonlocal best_left, best_right
-            if r - l < best_right - best_left:
-                best_left, best_right = l, r
-        
-        while right < len(s):
+        best_left, best_right = 0, float('inf')
+
+        def add():
+            nonlocal have
             window[s[right]] += 1
             if window[s[right]] == target[s[right]]:
                 have += 1
-            
-            while left <= right and have == need:
+        def remove():
+            nonlocal have
+            if window[s[left]] == target[s[left]]:
+                have -= 1
+            window[s[left]] -= 1
+        def windowValid():
+            return have == need
+        def updateAnswer(l: int, r: int):
+            nonlocal best_left, best_right
+            if r - l < best_right - best_left:
+                best_left, best_right = l, r
+
+        while right < len(s):
+            add()
+
+            while left <= right and windowValid():
                 updateAnswer(left, right)
-                if window[s[left]] == target[s[left]]:
-                    have -= 1
-                window[s[left]] -= 1
+                remove()
                 left += 1
             right += 1
-        return "" if best_right == float('inf') else s[best_left:best_right + 1]
-        
+        print(locals())
+        return "" if best_right == float('inf') else s[best_left: best_right + 1]
