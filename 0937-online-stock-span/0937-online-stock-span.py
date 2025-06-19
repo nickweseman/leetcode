54140d01067fdfb324333fae00ac39c831978cc9
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-19 15:16:51 -0500

    Time: 70 ms (42.14%), Space: 23 MB (34.08%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -2,7 +2,7 @@ class StockSpanner:
 
     def __init__(self):
         self.stack = []
-        
+
     def next(self, price: int) -> int:
         span = 1
         while self.stack and self.stack[-1][0] <= price:
