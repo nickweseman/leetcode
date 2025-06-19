638af9b4d2ad6f6832da8797df8878b1e139293c
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-19 10:13:29 -0500

    Time: 35 ms (97.08%), Space: 18 MB (51.02%) - LeetHub

--- 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
@@ -2,6 +2,7 @@ class Solution:
     def makeSmallestPalindrome(self, s: str) -> str:
         left, right = 0, len(s) - 1
         ss = list(s)
+
         while left < right:
             if ss[left] < ss[right]:
                 ss[right] = ss[left]
