8ccac262b92dc26125c8d339b78a980c40fe355a
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-20 08:11:33 -0500

    Time: 0 ms (100%), Space: 17.8 MB (80.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,9 +7,6 @@ class Solution:
             if char in mappings:
                 stack.append(char)
             else:
-                if not stack:
-                    return False
-                ch = stack.pop()
-                if ch not in mappings or mappings[ch] != char:
+                if not stack or mappings[stack.pop()] != char:
                     return False
         return len(stack) == 0
