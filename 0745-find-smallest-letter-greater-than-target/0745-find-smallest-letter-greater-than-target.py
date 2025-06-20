e1bdae09968f329fd682600647910299e17e110e
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-20 15:12:49 -0500

    Time: 0 ms (100%), Space: 19.2 MB (6.41%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.py
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.py
@@ -1,6 +1,7 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        left, right = 0, len(letters)
+        n = len(letters)
+        left, right = 0, n
 
         while left < right:
             mid = (left + right) // 2
@@ -8,4 +9,4 @@ class Solution:
                 left = mid + 1
             else:
                 right = mid
-        return letters[0] if left in (0, len(letters)) else letters[left]
+        return letters[left % n] # handles the case where left = n gracefully
