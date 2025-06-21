ee2c99f7a0a57e9440c7c44531d098e68d9251f4
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-21 08:02:18 -0500

    Time: 0 ms (100%), Space: 17.9 MB (9.1%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
@@ -3,20 +3,16 @@ class Solution:
         window = collections.defaultdict(int)
         left = right = 0
         substrings = 0
-        distinct = 0
 
         while right < len(s):
             window[s[right]] += 1
-            if window[s[right]] == 1:
-                distinct += 1
 
             if right - left + 1 > 3:
-                if window[s[left]] == 1:
-                    distinct -= 1
                 window[s[left]] -= 1
+                if window[s[left]] == 0:
+                    del window[s[left]]
                 left += 1
-            if right - left + 1 == 3 and distinct == 3:
+            if right - left + 1 == 3 == len(window):
                 substrings += 1
             right += 1
         return substrings
-            
