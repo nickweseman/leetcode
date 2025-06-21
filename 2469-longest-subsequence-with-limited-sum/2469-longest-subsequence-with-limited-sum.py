cdf40ce6c711812db3629799db0c1be51674b964
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-21 09:52:05 -0500

    Time: 4 ms (90.94%), Space: 18.2 MB (45.1%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -2,13 +2,9 @@ class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
         prefix_sums = list(itertools.accumulate(nums))
-        answer = []
-        
+        result = []
+
         for q in queries:
-            # `bisect_right` finds the insertion point for `q`, which is
-            # equivalent to counting the number of prefix sums <= q.
-            # This count is the maximum size of the subsequence.
-            # Example: q=10, prefix_sums=[1,3,7,12]. bisect_right returns 3.
             size = bisect.bisect_right(prefix_sums, q)
-            answer.append(size)
-        return answer
+            result.append(size)
+        return result
