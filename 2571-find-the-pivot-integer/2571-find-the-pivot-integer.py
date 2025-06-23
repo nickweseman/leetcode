5155c884d07d44ebac90f2af4c39be235e0e8f11
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-23 06:29:52 -0500

    Time: 0 ms (100%), Space: 18 MB (16.96%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -1,15 +1,11 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        # From the derivation, we know x^2 = (n^2 + n) / 2
-        # Let's calculate the value that x^2 should be.
-        target = (n * n + n) / 2
-        
-        # Take the square root to find a potential x.
+        # x^2 = (n^2 + n) / 2
+        target = (n ** 2 + n) / 2
         x = math.sqrt(target)
         
         # Check if x is a whole number. 
-        # A simple way is to see if it has no fractional part.
-        if x % 1 == 0:
+        if x == int(x):
             return int(x)
         else:
             return -1
