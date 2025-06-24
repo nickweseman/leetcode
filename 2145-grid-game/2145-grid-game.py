2d5678a60c3abb4775532c28bd7d3cc1287adb52
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-24 17:18:28 -0500

    Time: 91 ms (58.27%), Space: 29.6 MB (73.87%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -2,7 +2,7 @@ class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         top_row_right_sum = sum(grid[0])
         bottom_row_left_sum = 0
-        lowest_score = float('inf')
+        robot_2_lowest_score = float('inf')
 
         for col in range(len(grid[0])):
             top_row_right_sum -= grid[0][col]
@@ -13,7 +13,7 @@ class Solution:
             robot2_score = max(top_row_right_sum, bottom_row_left_sum)
             
             # Robot 1 wants to minimize Robot 2's score
-            lowest_score = min(lowest_score, robot2_score)
+            robot_2_lowest_score = min(robot_2_lowest_score, robot2_score)
 
             bottom_row_left_sum += grid[1][col]
-        return lowest_score
+        return robot_2_lowest_score
