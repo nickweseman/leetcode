58fa63fbc97dc67a9e3ab2334ba23a0fe3e9a40d
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-24 07:02:35 -0500

    Time: 343 ms (15.4%), Space: 29 MB (29.43%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -1,20 +1,19 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         score = max_score = 0
-        left = right = 0
         window = collections.defaultdict(int)
+        left = right = 0
 
         while right < len(nums):
             window[nums[right]] += 1
             score += nums[right]
 
-            while len(window) < right - left + 1:
+            while right - left + 1 > len(window):
                 window[nums[left]] -= 1
+                score -= nums[left]
                 if window[nums[left]] == 0:
                     del window[nums[left]]
-                score -= nums[left]
                 left += 1
             max_score = max(max_score, score)
             right += 1
         return max_score
-                
