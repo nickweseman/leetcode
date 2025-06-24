0d3440da5bf141c1bbe380949e79b0825cb096da
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-24 06:27:01 -0500

    Time: 0 ms (100%), Space: 18.7 MB (44.91%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -4,6 +4,7 @@ class Solution:
 
         while left <= right:
             mid = (left + right) // 2
+
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
