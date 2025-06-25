b59537030196c6e10caba7e52ef98e6e3d72101a
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-25 17:35:42 -0500

    Time: 55 ms (77.28%), Space: 18.1 MB (80.39%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -1,21 +1,17 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         left_sum, right_sum = 0, customers.count("Y")
-        min_penalty = math.inf
-        min_index = -1
+        min_penalty = right_sum
+        min_index = 0
 
         for i, num in enumerate(customers):
-            curr_penalty = left_sum + right_sum
-            if curr_penalty < min_penalty:
-                min_penalty = curr_penalty
-                min_index = i
-            
             if num == "Y":
                 right_sum -= 1
             else:
                 left_sum += 1
-        curr_penalty = left_sum + right_sum
-        if curr_penalty < min_penalty:
-            min_penalty = curr_penalty
-            min_index = len(customers)
+            
+            curr_penalty = left_sum + right_sum
+            if curr_penalty < min_penalty:
+                min_penalty = curr_penalty
+                min_index = i + 1
         return min_index
