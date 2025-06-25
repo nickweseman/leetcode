95d595576ab08e2226dde6e6e8413b491938adec
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-25 16:51:45 -0500

    Time: 79 ms (87.02%), Space: 29.7 MB (52.67%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -1,19 +1,16 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        top_row_right_sum = sum(grid[0])
-        bottom_row_left_sum = 0
-        robot_2_lowest_score = float('inf')
+        right_top = sum(grid[0])
+        left_bottom = 0
+        lowest_robot_2_score = float('inf')
 
         for col in range(len(grid[0])):
-            top_row_right_sum -= grid[0][col]
+            right_top -= grid[0][col]
 
-            # Robot 2's best choice is max of:
-            # - Going all top (what's left on top row)
-            # - Going all bottom (what we've seen on bottom row)
-            robot2_score = max(top_row_right_sum, bottom_row_left_sum)
-            
-            # Robot 1 wants to minimize Robot 2's score
-            robot_2_lowest_score = min(robot_2_lowest_score, robot2_score)
+            robot2_score = max(right_top, left_bottom)
+            lowest_robot_2_score = min(lowest_robot_2_score, robot2_score)
 
-            bottom_row_left_sum += grid[1][col]
-        return robot_2_lowest_score
+            left_bottom += grid[1][col]
+        return lowest_robot_2_score
+        
+        
