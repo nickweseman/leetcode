e94d3d51ad6b0b72cd05f25dacc44027cb5b4cd0
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-25 12:31:23 -0500

    Time: 4 ms (75.58%), Space: 17.9 MB (38.03%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -1,11 +1,12 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        left_sum = 0
-        right_sum = (n * (n + 1)) // 2
+        left_sum, right_sum = 0, (n * (n + 1)) // 2
 
-        for i in range(1, n + 1):
+        for i in range(n + 1):
             right_sum -= i
+
             if left_sum == right_sum:
                 return i
+
             left_sum += i
         return -1
