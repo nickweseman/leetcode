0ebcdee0f34462a67b0f69165b11c66049ea5c12
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-25 12:57:08 -0500

    Time: 0 ms (100%), Space: 18 MB (26.07%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
@@ -1,9 +1,9 @@
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
         avgs = set()
-        nums.sort()
         left, right = 0, len(nums) - 1
-
+        nums.sort()
+        
         while left < right:
             avgs.add((nums[left] + nums[right]) / 2)
             left += 1
