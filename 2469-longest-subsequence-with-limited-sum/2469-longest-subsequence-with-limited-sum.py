8d345be1210020aff0d39f9c1cae79be232dfa8d
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-26 17:34:22 -0500

    Time: 12 ms (72.21%), Space: 18.1 MB (44.61%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -2,10 +2,11 @@ class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         answer = []
         nums.sort()
-        prefix_sum = [0] * (len(nums) + 1)
-        for i in range(1,len(nums) + 1):
-            prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1]
-        print(locals())
+        
+        prefix_sum = [0]
+        for num in nums:
+            prefix_sum.append(prefix_sum[-1] + num)
+
         for q in queries:
             left, right = 0, len(prefix_sum)
 
