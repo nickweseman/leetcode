c3e826c42753577010e1b0fc98618073fedce0c1
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-26 16:43:37 -0500

    Time: 115 ms (43.78%), Space: 17.9 MB (63.22%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -3,12 +3,11 @@ class Solution:
         total_substrings = (len(s) * (len(s) + 1)) // 2
         substrings = 0
         left = right = 0
-        window = {"a":0, "b":0, "c":0}
+        window = {'a':0, 'b':0, 'c':0}
 
         while right < len(s):
             window[s[right]] += 1
-
-            while window["a"] > 0 and window["b"] > 0 and window["c"] > 0:
+            while window['a'] > 0 and window['b'] > 0 and window['c'] > 0:
                 window[s[left]] -= 1
                 left += 1
             substrings += right - left + 1
