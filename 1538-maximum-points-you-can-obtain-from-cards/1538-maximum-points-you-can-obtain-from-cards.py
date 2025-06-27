f70772ba00aefb70d506dde29114807ae4fe8123
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-27 09:24:51 -0500

    Time: 20 ms (84.72%), Space: 28.1 MB (41.41%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -4,7 +4,7 @@ class Solution:
         max_score = right_sum
 
         for i in range(k):
-            right_sum -= cardPoints[-(k-i)]
+            right_sum -= cardPoints[-(k - i)]
             left_sum += cardPoints[i]
             max_score = max(max_score, left_sum + right_sum)
         return max_score
