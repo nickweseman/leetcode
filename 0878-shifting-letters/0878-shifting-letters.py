1970f2071198bb05418ab71ee517a37ba99233a2
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-27 09:41:26 -0500

    Time: 78 ms (79.7%), Space: 29.2 MB (71.31%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.py
+++ 0878-shifting-letters/0878-shifting-letters.py
@@ -2,12 +2,12 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         total_shifts = 0
         n = len(s)
-
         ss = list(s)
+
         for i in reversed(range(n)):
             total_shifts += shifts[i]
 
             diff = ord(ss[i]) - ord('a')
             diff = (diff + total_shifts) % 26
-            ss[i] = chr(ord('a') + diff)
+            ss[i] = chr(diff + ord('a'))
         return "".join(ss)
