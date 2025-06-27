723b50a9bb93cf626a8447ff2fa9798deb41e56f
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-27 11:25:34 -0500

    Time: 47 ms (49.64%), Space: 67.8 MB (40.29%) - LeetHub

--- 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
+++ 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
@@ -2,18 +2,21 @@ class Solution:
     def canEat(self, candiesCount: List[int], queries: List[List[int]]) -> List[bool]:
         answer = []
 
-        prefix_sum = [0]
-        for count in candiesCount:
-            prefix_sum.append(prefix_sum[-1] + count)
+        for i in range(1, len(candiesCount)):
+            candiesCount[i] += candiesCount[i - 1]
         
         for favorite_type, favorite_day, daily_cap in queries:
-            max_candies_eaten = (favorite_day + 1) * daily_cap
-            first_favorite_candy = prefix_sum[favorite_type] + 1
-            not_too_late = max_candies_eaten >= first_favorite_candy
+            max_eaten = (favorite_day + 1) * daily_cap  # eat daily_cap per day
+            min_eaten = favorite_day + 1                # eat 1 per day
 
-            min_candies_eaten = favorite_day + 1
-            last_favorite_candy = prefix_sum[favorite_type + 1]
-            not_too_early = min_candies_eaten <= last_favorite_candy
+            if favorite_type == 0:
+                first_favorite = 1
+            else:
+                first_favorite = candiesCount[favorite_type - 1] + 1
+            last_favorite = candiesCount[favorite_type]  # this represents the prefix sum for the next favorite type, can't be bigger than this 
+
+            not_too_late = max_eaten >= first_favorite
+            not_too_early = min_eaten <= last_favorite
             
             answer.append(not_too_late and not_too_early)
         return answer
