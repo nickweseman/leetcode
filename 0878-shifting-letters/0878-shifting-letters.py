f9d35a2854c473e0ad0dd3ffc79bd685bcc6c751
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-26 20:01:08 -0500

    Time: 75 ms (85.74%), Space: 29.1 MB (85.23%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.py
+++ 0878-shifting-letters/0878-shifting-letters.py
@@ -1,15 +1,13 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         total_shifts = 0
-        for i in reversed(range(len(shifts))):
+        n = len(s)
+
+        ss = list(s)
+        for i in reversed(range(n)):
             total_shifts += shifts[i]
-            shifts[i] = total_shifts
-        
-        ss = []
-        for i, c in enumerate(s):
-            start_pos = ord(c) - ord('a')
-            end_pos = (start_pos + shifts[i]) % 26
-            
-            new_char = chr(end_pos + ord('a'))
-            ss.append(new_char)
+
+            diff = ord(ss[i]) - ord('a')
+            diff = (diff + total_shifts) % 26
+            ss[i] = chr(ord('a') + diff)
         return "".join(ss)
