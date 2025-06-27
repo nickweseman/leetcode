073295aa9cd29d80d5b43b6d75e8fa93f570db64
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-27 13:13:28 -0500

    Time: 15 ms (94.86%), Space: 49.7 MB (75.39%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -0,0 +1,15 @@
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        ans = []
+        vowels = set("aeiou")
+
+        prefix_sum = [0]
+        for word in words:
+            if word[0] in vowels and word[-1] in vowels:
+                prefix_sum.append(prefix_sum[-1] + 1)
+            else:
+                prefix_sum.append(prefix_sum[-1])
+        
+        for left, right in queries:
+            ans.append(prefix_sum[right + 1] - prefix_sum[left])
+        return ans
