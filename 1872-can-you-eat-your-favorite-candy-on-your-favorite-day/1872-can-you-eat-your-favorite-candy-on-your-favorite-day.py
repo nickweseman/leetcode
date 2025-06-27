8ce98dcd7ec3130289ec33dd7fef657e1a990241
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-27 11:38:00 -0500

    Time: 45 ms (52.52%), Space: 67.9 MB (40.29%) - LeetHub

--- 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
+++ 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
@@ -6,17 +6,16 @@ class Solution:
             candiesCount[i] += candiesCount[i - 1]
         
         for favorite_type, favorite_day, daily_cap in queries:
-            max_eaten = (favorite_day + 1) * daily_cap  # eat daily_cap per day
             min_eaten = favorite_day + 1                # eat 1 per day
+            max_eaten = (favorite_day + 1) * daily_cap  # eat daily_cap per day
 
             if favorite_type == 0:
                 first_favorite = 1
             else:
                 first_favorite = candiesCount[favorite_type - 1] + 1
-            last_favorite = candiesCount[favorite_type]  # this represents the prefix sum for the next favorite type, can't be bigger than this 
-
-            not_too_late = max_eaten >= first_favorite
-            not_too_early = min_eaten <= last_favorite
             
-            answer.append(not_too_late and not_too_early)
+            last_favorite = candiesCount[favorite_type]
+
+            can_reach = max_eaten >= first_favorite and min_eaten <= last_favorite
+            answer.append(can_reach)
         return answer
