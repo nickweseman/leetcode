7d72aee7e2bc01c4237caba62e47241533366471
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-26 19:21:06 -0500

    Time: 602 ms (80.05%), Space: 34.2 MB (9.3%) - LeetHub

--- 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        left_sum, right_sum = 0, sum(nums)
         result = []
+        left_sum, right_sum = 0, sum(nums)
         n = len(nums)
 
         for i, num in enumerate(nums):
@@ -10,6 +10,6 @@ class Solution:
             left_total = (num * i) - left_sum
             right_total = right_sum - (n - i - 1) * num
             result.append(left_total + right_total)
-
+            
             left_sum += num
         return result
