b0708b9626d64609a3ec86a1f3dc29dddb6bd134
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-28 17:00:21 -0500

    Time: 4 ms (19.6%), Space: 17.8 MB (77.89%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,9 +1,10 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
+        answer = prices[:]
         stack = []
 
         for i, price in enumerate(prices):
             while stack and prices[stack[-1]] >= price:
-                prices[stack.pop()] -= price
+                answer[stack.pop()] -= price
             stack.append(i)
-        return prices
+        return answer
