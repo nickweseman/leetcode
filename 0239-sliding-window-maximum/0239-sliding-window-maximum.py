f84c693040872dc2f85449e70acfa417cac7e523
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-28 17:33:05 -0500

    Time: 308 ms (19.02%), Space: 31.2 MB (70.68%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,26 +1,27 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         window = collections.deque()
+        answer = []
         left = right = 0
-        result = []
-
+        
         def add() -> None:
             while window and nums[window[-1]] < nums[right]:
                 window.pop()
             window.append(right)
+
         def remove() -> None:
-            if window and left == window[0]:
+            if window and window[0] == left:
                 window.popleft()
-        def validWindow() -> bool:
-            return True
-        def updateAnswer() -> None:
-            result.append(nums[window[0]])
+
+        def update_answer() -> None:
+            answer.append(nums[window[0]])
+
         while right < len(nums):
             add()
             if right - left + 1 > k:
                 remove()
                 left += 1
-            if right - left + 1 == k and validWindow():
-                updateAnswer()
+            if right - left + 1 == k:
+                update_answer()
             right += 1
-        return result
+        return answer
