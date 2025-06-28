c35968e77bc3ba30a80efeccd765fb19b9a1a9d9
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-28 17:07:58 -0500

    Time: 31 ms (44.25%), Space: 17.9 MB (62.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,18 +1,15 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         target = collections.Counter(s1)
-        window = collections.Counter()
-
+        window = collections.defaultdict(int)
+        left = right = 0
         have = 0
         need = len(target)
-
-        left = right = 0
 
         while right < len(s2):
             window[s2[right]] += 1
             if window[s2[right]] == target[s2[right]]:
                 have += 1
-            
             if right - left + 1 > len(s1):
                 if window[s2[left]] == target[s2[left]]:
                     have -= 1
