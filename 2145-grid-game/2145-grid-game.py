315ed1411ec9c44b328556a0003dd114fc879cdd
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-29 08:50:41 -0500

    Time: 75 ms (90.77%), Space: 29.5 MB (85.12%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -1,13 +1,14 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         bottom_left, top_right = 0, sum(grid[0])
-        lowest_robot_2_score = math.inf
+        lowest_robot2_score = math.inf
 
         for col in range(len(grid[0])):
             top_right -= grid[0][col]
 
-            robot_2_score = max(top_right, bottom_left)
-            lowest_robot_2_score = min(lowest_robot_2_score, robot_2_score)
+            robot2_score = max(top_right, bottom_left)
+            lowest_robot2_score = min(lowest_robot2_score, robot2_score)
 
             bottom_left += grid[1][col]
-        return lowest_robot_2_score
+
+        return lowest_robot2_score
