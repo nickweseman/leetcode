34de5b7e19d02d4a6f0a48b9f7c620b470b7d633
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-29 07:17:02 -0500

    Time: 0 ms (100%), Space: 17.7 MB (89.11%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         left_sum, right_sum = 0, s.count("1")
-        max_score = float('-inf')
+        max_score = 0
 
-        for i, num in enumerate(s[:-1]):
-            if num == "1":
+        for c in s[:-1]:
+            if c == "1":
                 right_sum -= 1
             else:
                 left_sum += 1
             max_score = max(max_score, left_sum + right_sum)
         return max_score
-            
