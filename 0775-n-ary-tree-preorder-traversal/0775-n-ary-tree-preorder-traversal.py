81ba68c57e9965d130ac111c177a193d581de36a
Nick Weseman <trumpernickel2@yahoo.com>
2025-06-29 10:34:56 -0500

    Time: 55 ms (18.64%), Space: 19.4 MB (47.27%) - LeetHub

---
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        result = []
+        def dfs(node) -> None:
+            result.append(node.val)
+            for child in node.children:
+                dfs(child)
+        dfs(root)
+        return result
