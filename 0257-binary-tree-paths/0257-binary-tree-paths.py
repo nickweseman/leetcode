e49ac171811b9059caaecb1847cc5bc0440edae6
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-02 17:48:27 -0500

    Time: 0 ms (100%), Space: 17.8 MB (70.29%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -13,10 +13,8 @@ class Solution:
             path.append(str(node.val))
             if not node.left and not node.right:
                 result.append("->".join(path))
-            else:
-                dfs(node.left, path)
-                dfs(node.right, path)
+            dfs(node.left, path)
+            dfs(node.right, path)
             path.pop()
-        if root:
-            dfs(root, [])
+        dfs(root, [])
         return result
