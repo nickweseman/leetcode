c901b1482cad2a6ce4e99722d254dc9b4ddfb918
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-02 18:15:10 -0500

    Time: 411 ms (5.21%), Space: 45.8 MB (5.03%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
@@ -6,30 +6,30 @@
 #         self.right = right
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        if not root:
-            return []
         subtrees = collections.defaultdict(int)
         result = []
-        def dfs(n) -> None:
-            if n:
-                subtrees[self.serialize(n)] += 1
-                if subtrees[self.serialize(n)] == 2:
-                    result.append(n)
-                dfs(n.left)
-                dfs(n.right)
+
+        def dfs(n):
+            if not n:
+                return
+            s = self.serialize(n)
+            subtrees[s] += 1
+            if subtrees[s] == 2:
+                result.append(n)
+            dfs(n.left)
+            dfs(n.right)
         dfs(root)
         return result
-        
+
     def serialize(self, node) -> str:
         result = []
-        def preorder(n) -> None:
+        def dfs(n) -> None:
             if not n:
                 result.append("#")
                 return
-            result.append(str(n.val))
-            preorder(n.left)
-            preorder(n.right)
-        preorder(node)
+            else:
+                result.append(str(n.val))
+                dfs(n.left)
+                dfs(n.right)
+        dfs(node)
         return "," + ",".join(result) + ","
-
-        
