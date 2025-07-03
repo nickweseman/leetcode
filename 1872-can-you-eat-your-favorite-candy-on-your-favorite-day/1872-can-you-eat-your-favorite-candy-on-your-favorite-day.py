06307cd226f43a9cd983e08eb2a9b831293c3868
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-03 07:10:40 -0500

    Time: 43 ms (63.23%), Space: 67.8 MB (44.52%) - LeetHub

--- 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
+++ 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
@@ -1,8 +1,9 @@
 class Solution:
     def canEat(self, candiesCount: List[int], queries: List[List[int]]) -> List[bool]:
-        answer = []
+        n = len(candiesCount)
+        result = []
 
-        for i in range(1, len(candiesCount)):
+        for i in range(1, n):
             candiesCount[i] += candiesCount[i - 1]
         
         for favorite_type, favorite_day, daily_cap in queries:
@@ -16,5 +17,5 @@ class Solution:
             last_favorite = candiesCount[favorite_type]
 
             can_reach = min_eaten <= last_favorite and max_eaten >= first_favorite
-            answer.append(can_reach)
-        return answer
+            result.append(can_reach)
+        return result
