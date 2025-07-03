df374b68d686df335bc8889b00f7b67c41f16951
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-02 20:24:07 -0500

    Time: 3 ms (8.07%), Space: 18 MB (12.89%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,10 +6,13 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node1, node2) -> bool:
+        if not root:
+            return True
+        def isMirror(node1, node2) -> bool:
             if not node1 and not node2:
                 return True
             if not node1 or not node2 or node1.val != node2.val:
                 return False
-            return dfs(node1.left, node2.right) and dfs(node1.right, node2.left)
-        return dfs(root.left, root.right)
+            return isMirror(node1.left, node2.right) and isMirror(node2.left, node1.right)
+        return isMirror(root.left, root.right)
+            
