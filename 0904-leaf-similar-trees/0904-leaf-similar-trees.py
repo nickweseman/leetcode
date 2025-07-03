013fb7b3e35cb934653dba44ecfe3962da5c4fed
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-02 19:33:21 -0500

    Time: 0 ms (100%), Space: 17.7 MB (74.27%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -6,18 +6,20 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def leaves(root) -> List[TreeNode]:
+        def tree_iter(root) -> Iterator[int]:
             if not root:
-                return []
+                return
             stack = [root]
-            result = []
             while stack:
                 curr = stack.pop()
-                if not curr.right and not curr.left:
-                    result.append(curr.val)
-                if curr.right:
-                    stack.append(curr.right)
-                if curr.left:
-                    stack.append(curr.left)
-            return result
-        return leaves(root1) == leaves(root2)
+                if not curr.left and not curr.right:
+                    yield curr.val
+                else:
+                    if curr.left:
+                        stack.append(curr.left)
+                    if curr.right:
+                        stack.append(curr.right)
+        for node1, node2 in itertools.zip_longest(tree_iter(root1), tree_iter(root2), fillvalue=object()):
+            if node1 != node2:
+                return False
+        return True
