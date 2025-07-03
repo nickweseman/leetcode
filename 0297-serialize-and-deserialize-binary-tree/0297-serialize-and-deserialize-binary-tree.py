9feb3edaa4f6f094b17e2400f15dd00d89c3d19e
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-03 06:26:02 -0500

    Time: 77 ms (90.77%), Space: 21.5 MB (30.22%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -6,6 +6,7 @@
 #         self.right = None
 
 class Codec:
+
     def serialize(self, root):
         result = []
         def dfs(node) -> None:
@@ -19,7 +20,6 @@ class Codec:
         return ",".join(result)
     def deserialize(self, data):
         values_iter = iter(data.split(","))
-
         def build_tree() -> TreeNode:
             v = next(values_iter)
             if v == "#":
@@ -29,3 +29,9 @@ class Codec:
             node.right = build_tree()
             return node
         return build_tree()
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
