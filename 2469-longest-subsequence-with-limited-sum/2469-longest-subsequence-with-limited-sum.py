2dbe60cd0435200fd0c7f1020d9caa17d6523d4d
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-04 06:36:14 -0500

    Time: 4 ms (90.81%), Space: 18.1 MB (68.15%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -5,12 +5,5 @@ class Solution:
         prefix_sums = list(itertools.accumulate(nums))
         
         for q in queries:
-            left, right = 0, len(prefix_sums)
-            while left < right:
-                mid = (left + right) // 2
-                if prefix_sums[mid] <= q:
-                    left = mid + 1
-                else:
-                    right = mid
-            answer.append(left)
+            answer.append(bisect.bisect_right(prefix_sums, q))
         return answer
