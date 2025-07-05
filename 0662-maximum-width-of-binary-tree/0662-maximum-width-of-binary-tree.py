95610073026a264b6d68eb6ffa9c152a88ccfda1
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-05 12:26:58 -0500

    Time: 6 ms (28.13%), Space: 18.8 MB (34.25%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = collections.deque([(root, 1)])
+        max_width = -math.inf
+        while queue:
+            leftmost_pos = queue[0][1]
+            rightmost_pos = queue[-1][1]
+            max_width = max(max_width, rightmost_pos - leftmost_pos + 1)
+            for _ in range(len(queue)):
+                node, pos = queue.popleft()
+                if node.left:
+                    queue.append((node.left, 2 * pos))
+                if node.right:
+                    queue.append((node.right, 2 * pos + 1))
+        return max_width
