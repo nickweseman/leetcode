e1e236259e42fee836bd3b02bc60779870e194a4
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-06 07:14:09 -0500

    Time: 0 ms (100%), Space: 17.7 MB (60.36%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
@@ -4,9 +4,7 @@ class Solution:
 
         for i, num in enumerate(nums):
             right_sum -= num
-
             if left_sum == right_sum:
                 return i
-            
             left_sum += num
         return -1
