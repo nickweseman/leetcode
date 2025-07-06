eb1825d47b5f3266bb5bdf287c4b2817f14f3bf0
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-06 10:54:29 -0500

    Time: 158 ms (9.02%), Space: 23 MB (57.22%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,10 +1,9 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        def helper(kk: int) -> int:
+        def num_arrays(kk) -> int:
             odds = 0
             left = right = 0
             subarrays = 0
-
             while right < len(nums):
                 if nums[right] % 2 == 1:
                     odds += 1
@@ -15,4 +14,4 @@ class Solution:
                 subarrays += right - left + 1
                 right += 1
             return subarrays
-        return helper(k) - helper(k - 1)
+        return num_arrays(k) - num_arrays(k - 1)
