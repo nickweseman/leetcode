e3f5eef83dbd8b28d1f3821f95616ff4ba0b7306
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-06 08:19:37 -0500

    Time: 23 ms (47.43%), Space: 32.2 MB (30.48%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.py
@@ -1,13 +1,12 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        answer = []
-
         for i in range(1, len(arr)):
-            arr[i] ^= arr[i - 1] 
-        
+            arr[i] ^= arr[i-1]
+        result = []
         for left, right in queries:
             if left == 0:
-                answer.append(arr[right])
+                result.append(arr[right])
             else:
-                answer.append(arr[right] ^ arr[left - 1])
-        return answer
+                result.append(arr[right] ^ arr[left - 1])
+        return result
+            
