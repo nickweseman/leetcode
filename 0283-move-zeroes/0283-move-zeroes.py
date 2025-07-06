dc4578bbf90f51c0242b613405b4bbd4a5bcaf5b
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-06 08:26:49 -0500

    Time: 7 ms (43.68%), Space: 18.8 MB (78.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,10 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         read = write = 0
-
         while read < len(nums):
             if nums[read] != 0:
                 nums[read], nums[write] = nums[write], nums[read]
                 write += 1
             read += 1
+            
         
