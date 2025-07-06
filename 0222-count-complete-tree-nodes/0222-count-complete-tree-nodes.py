dbd458107a115f74265dbb3ab684ef9f91ae03f4
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-06 11:18:46 -0500

    Time: 0 ms (100%), Space: 23.3 MB (81.83%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -8,15 +8,15 @@ class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        def get_height(node) -> int:
+        def get_height(node):
             height = 0
             while node:
                 height += 1
                 node = node.left
             return height
-        left_height = get_height(root.left)
-        right_height = get_height(root.right)
-        if left_height == right_height: # left is a perfect subtree
+        left_height, right_height = get_height(root.left), get_height(root.right)
+        if left_height == right_height:
             return (1 << left_height) + self.countNodes(root.right)
-        else:                          # right is a perfect subtree
+        else:
             return (1 << right_height) + self.countNodes(root.left)
+            
