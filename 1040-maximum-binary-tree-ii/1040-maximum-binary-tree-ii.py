c8c523f78367dae9c8bec98aa42766fe4f739cbd
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-07 09:49:24 -0500

    Time: 0 ms (100%), Space: 17.8 MB (38.5%) - LeetHub

---
+++ 1040-maximum-binary-tree-ii/1040-maximum-binary-tree-ii.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoMaxTree(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if val > root.val:
+            return TreeNode(val, left=root)
+        curr = root
+        while curr.right and curr.right.val > val:
+            curr = curr.right
+        node = TreeNode(val)
+        node.left = curr.right
+        curr.right = node
+        return root
