36598c981c81f9aa3aba3f7b3e28caa834caf9f6
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 08:27:21 -0500

    Time: 95 ms (43.89%), Space: 18.4 MB (16.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,20 +5,18 @@ class Solution:
         have = 0
         need = len(target)
         left = right = 0
-        min_window = ""
-        min_length = math.inf
+        best_left, best_right = 0, math.inf
 
         while right < len(s):
             window[s[right]] += 1
             if window[s[right]] == target[s[right]]:
                 have += 1
             while left <= right and have == need:
-                if right - left + 1 < min_length:
-                    min_length = right - left + 1
-                    min_window = s[left:right+1]
+                if right - left < best_right - best_left:
+                    best_left, best_right = left, right
                 if window[s[left]] == target[s[left]]:
                     have -= 1
                 window[s[left]] -= 1
                 left += 1
             right += 1
-        return min_window
+        return "" if best_right == math.inf else s[best_left:best_right+1]
