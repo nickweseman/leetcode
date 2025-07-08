64166cecc2831c25c393fc957eb4512e0386d1fa
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 15:51:47 -0500

    Time: 0 ms (100%), Space: 21.1 MB (91.99%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -7,33 +7,23 @@
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         modes = []
-        current_freq = max_freq = 0
-        current_val = None
-
+        current_value = None
+        current_count = max_freq = 0
         def dfs(node) -> None:
-            nonlocal current_freq, max_freq, current_val, modes
-            if node:
-                dfs(node.left)
-                print(f"{node.val} {current_val}")
-                if node.val == current_val:
-                    print(f"got here")
-                    current_freq += 1
-                    if current_freq > max_freq:
-                        modes = [node.val]
-                        max_freq = max(max_freq, current_freq)
-                    elif current_freq == max_freq:
-                        modes.append(node.val)
-                    print(f"got here {modes=}")
-                else:
-                    current_freq = 1
-                    if current_freq > max_freq:
-                        modes = [node.val]
-                        max_freq = max(max_freq, current_freq)
-                    elif current_freq == max_freq:
-                        modes.append(node.val)
-                    current_val = node.val
-                dfs(node.right)
+            nonlocal current_value, current_count, modes, max_freq
+            if not node:
+                return
+            dfs(node.left)
+            if node.val != current_value:
+                current_value = node.val
+                current_count = 1
+            else:
+                current_count += 1
+            if current_count > max_freq:
+                modes = [node.val]
+                max_freq = current_count
+            elif current_count == max_freq:
+                modes.append(node.val)
+            dfs(node.right)
         dfs(root)
-        print(f"got here2 {modes=}")
         return modes
-        
