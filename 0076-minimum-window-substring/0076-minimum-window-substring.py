d6183bc139e17cb88a49065077c96155b65a53cb
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 08:21:31 -0500

    Time: 159 ms (22.83%), Space: 18.6 MB (7.14%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,38 +2,23 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         window = collections.Counter()
         target = collections.Counter(t)
-
-        left = right = 0
-
         have = 0
         need = len(target)
+        left = right = 0
+        min_window = ""
+        min_length = math.inf
 
-        best_left, best_right = 0, float('inf')
-
-        def add():
-            nonlocal have
+        while right < len(s):
             window[s[right]] += 1
             if window[s[right]] == target[s[right]]:
                 have += 1
-        def remove():
-            nonlocal have
-            if window[s[left]] == target[s[left]]:
-                have -= 1
-            window[s[left]] -= 1
-        def windowValid():
-            return have == need
-        def updateAnswer(l: int, r: int):
-            nonlocal best_left, best_right
-            if r - l < best_right - best_left:
-                best_left, best_right = l, r
-
-        while right < len(s):
-            add()
-
-            while left <= right and windowValid():
-                updateAnswer(left, right)
-                remove()
+            while left <= right and have == need:
+                if right - left + 1 < min_length:
+                    min_length = right - left + 1
+                    min_window = s[left:right+1]
+                if window[s[left]] == target[s[left]]:
+                    have -= 1
+                window[s[left]] -= 1
                 left += 1
             right += 1
-        print(locals())
-        return "" if best_right == float('inf') else s[best_left: best_right + 1]
+        return min_window
