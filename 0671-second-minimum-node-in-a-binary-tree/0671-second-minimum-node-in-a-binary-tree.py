bc0054a6f28f3b13c9422a5c0294f9da1ecc469f
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 17:33:54 -0500

    Time: 0 ms (100%), Space: 17.6 MB (81.55%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
@@ -9,17 +9,15 @@ class Solution:
         if not root:
             return -1
         min1 = root.val
+        min2 = math.inf
         def dfs(node) -> int:
+            nonlocal min2
             if not node:
                 return math.inf
-            if not node.right and not node.left and node.val != min1:
-                if node.val != min1:
-                    return node.val
-                else:
-                    return math.inf
+            if node.val > min1:
+                min2 = min(min2, node.val)
             left = dfs(node.left)
             right = dfs(node.right)
-            return min(left, right)
-        min2 = dfs(root)
+        dfs(root)
         return -1 if min2 == math.inf else min2
         
