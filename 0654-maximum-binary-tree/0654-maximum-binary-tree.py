887305a686ab7be1b069f9024646af79f0590315
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 18:31:28 -0500

    Time: 23 ms (83.44%), Space: 18.2 MB (81.96%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -6,17 +6,12 @@
 #         self.right = right
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        def get_max_index(left, right) -> int:
-            maxx = -math.inf
-            max_index = -1
-            for i in range(left, right + 1):
-                if nums[i] > maxx:
-                    maxx = nums[i]
-                    max_index = i
-            return max_index
-        def create_tree(left, right) -> Optional[TreeNode]:
-            if left > right:
-                return None
-            max_i = get_max_index(left, right)
-            return TreeNode(val=nums[max_i], left=create_tree(left, max_i - 1), right=create_tree(max_i + 1, right))
-        return create_tree(0, len(nums) - 1)
+        stack = []
+        for num in nums:
+            curr = TreeNode(num)
+            while stack and stack[-1].val < num:
+                curr.left = stack.pop()
+            if stack:
+                stack[-1].right = curr
+            stack.append(curr)
+        return stack[0]
