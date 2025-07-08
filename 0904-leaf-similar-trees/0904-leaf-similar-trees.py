72c456fd87da1bcd2950a6f9b9ba067fbeb9a4e0
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 07:56:01 -0500

    Time: 0 ms (100%), Space: 18 MB (27.11%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -7,15 +7,13 @@
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         def get_leaves(node) -> Iterator[int]:
-            def dfs(n) -> Iterator[int]:
-                if n:
-                    if not n.right and not n.left:
-                        yield n.val
-                    yield from dfs(n.left)
-                    yield from dfs(n.right)
-            yield from dfs(node)
+            if not node:
+                return
+            if not node.left and not node.right:
+                yield node.val
+            yield from get_leaves(node.left)
+            yield from get_leaves(node.right)
         for leaf1, leaf2 in itertools.zip_longest(get_leaves(root1), get_leaves(root2), fillvalue=object()):
             if leaf1 != leaf2:
                 return False
         return True
-        
