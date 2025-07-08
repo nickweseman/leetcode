7ab0ba2e71ac02c255158a1c1a62cc5fcd659f67
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 09:03:43 -0500

    Time: 4 ms (65.82%), Space: 19.6 MB (89.17%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,40 +6,19 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        
-        def inorder_generator(node: Optional[TreeNode]):
-            """Yields nodes from smallest to largest."""
-            if not node:
-                return
-            yield from inorder_generator(node.left)
-            yield node
-            yield from inorder_generator(node.right)
-
-        def reverse_inorder_generator(node: Optional[TreeNode]):
-            """Yields nodes from largest to smallest."""
-            if not node:
-                return
-            yield from reverse_inorder_generator(node.right)
-            yield node
-            yield from reverse_inorder_generator(node.left)
-
-        # Create the two iterators
-        gen_l = inorder_generator(root)
-        gen_r = reverse_inorder_generator(root)
-        
-        # Get the first elements
-        left_ptr = next(gen_l)
-        right_ptr = next(gen_r)
-        
-        while left_ptr and right_ptr and left_ptr.val < right_ptr.val:
-            current_sum = left_ptr.val + right_ptr.val
-            
-            if current_sum == k:
+        goals = set()
+        if not root:
+            return False
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            if k - curr.val in goals:
                 return True
-            elif current_sum < k:
-                left_ptr = next(gen_l) # Get next smallest
-            else: # current_sum > k
-                right_ptr = next(gen_r) # Get next largest
-        
+            goals.add(curr.val)
+            curr = curr.right
         return False
         
