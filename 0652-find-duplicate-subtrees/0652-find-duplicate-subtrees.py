f5e0cb2f252644bbec9a56777787bcafd846a807
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-08 05:35:50 -0500

    Time: 7 ms (69.24%), Space: 24.8 MB (59.39%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
@@ -7,18 +7,16 @@
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
         subtrees = collections.defaultdict(int)
-        result = []
-        def serialize(node):
+        nodes = []
+        def serialize(node) -> str:
             if not node:
                 return "#"
             left = serialize(node.left)
             right = serialize(node.right)
-            # Create a tuple instead of string
-            subtree_id = (node.val, left, right)
-            subtrees[subtree_id] += 1
-            if subtrees[subtree_id] == 2:
-                result.append(node)
-            return subtree_id
+            s = f"{node.val},{left},{right}"
+            subtrees[s] += 1
+            if subtrees[s] == 2:
+                nodes.append(node)
+            return s
         serialize(root)
-        return result
-        
+        return nodes
