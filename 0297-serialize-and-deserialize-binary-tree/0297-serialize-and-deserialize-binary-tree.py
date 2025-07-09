e39b79d1c5f718234688ebec71fe9fc22a7e3fab
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-09 07:48:10 -0500

    Time: 89 ms (44.06%), Space: 21.1 MB (90.34%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -18,20 +18,24 @@ class Codec:
             dfs(node.right)
         dfs(root)
         return ",".join(result)
-    def deserialize(self, data):
-        values_iter = iter(data.split(","))
-        def build_tree() -> TreeNode:
-            v = next(values_iter)
-            if v == "#":
-                return
-            node = TreeNode(v)
-            node.left = build_tree()
-            node.right = build_tree()
-            return node
-        return build_tree()
-        
 
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
+    def deserialize(self, data):
+        values = data.split(",")
+        values_index = 0
+        
+        def dfs() -> Optional[TreeNode]:
+            nonlocal values_index
+            curr = values[values_index]
+            values_index += 1
+            if curr == "#":
+                return None
+            node = TreeNode(curr)
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+
+        
+        
+        
+        return TODO
