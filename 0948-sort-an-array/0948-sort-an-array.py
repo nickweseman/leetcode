6867009f1f33ce433077033b18b0b2f31761f246
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-10 18:35:29 -0500

    Time: 175 ms (80.89%), Space: 24.6 MB (62.53%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        MIN_VAL, MAX_VAL = -50_000, 50_000
+        count_size = MAX_VAL - MIN_VAL + 1
+        offset = -MIN_VAL
+        counts = [0] * count_size
+        for num in nums:
+            counts[num + offset] += 1 # shifts the range from [-50k, 50k] to [0, 100k] for array indexing
+        index = 0
+        for i in range(count_size):
+            count = counts[i]
+            for _ in range(count):
+                nums[index] = i - offset # shifts back from [0, 100k] to [-50k, 50k] for the actual values
+                index += 1
+        return nums
