78bb691e556cb6db388d6bf7f714716f15a9f99b
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-10 15:33:42 -0500

    Time: 6832 ms (5.02%), Space: 29.7 MB (12.43%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
@@ -1,25 +1,21 @@
 class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        nums = [int(x) for x in nums]
-        shuffle(nums)
-        
-        def part(lo, hi): 
-            """Return partition of nums[lo:hi]."""
-            i, j = lo+1, hi-1
-            while i <= j: 
-                if nums[i] < nums[lo]: i += 1
-                elif nums[lo] < nums[j]: j -= 1
-                else: 
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i += 1
-                    j -= 1
-            nums[lo], nums[j] = nums[j], nums[lo]
-            return j 
-        
-        lo, hi = 0, len(nums)
-        while lo < hi: 
-            mid = part(lo, hi)
-            if mid == len(nums)-k: return str(nums[mid])
-            elif mid < len(nums)-k: lo = mid + 1
-            else: hi = mid
-        
+    def kthLargestNumber(self, inums: List[str], k: int) -> str:
+        inums = [int(x) for x in inums]
+        k = len(inums) - k
+        def quick_select(l, r) -> int:
+            pivot_index = random.randint(l, r)
+            inums[r], inums[pivot_index] = inums[pivot_index], inums[r]
+            pivot = inums[r]
+            p = l
+            for i in range(l, r):
+                if inums[i] <= pivot:
+                    inums[i], inums[p] = inums[p], inums[i]
+                    p += 1
+            inums[p], inums[r] = inums[r], inums[p]
+            if p > k:
+                return quick_select(l, p - 1)
+            elif p < k:
+                return quick_select(p + 1, r)
+            else:
+                return inums[p]
+        return str(quick_select(0, len(inums) - 1))
