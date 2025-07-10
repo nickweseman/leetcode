d652aee2d5b04a4b02f78a3c7a7c9fb81a2acb27
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-10 18:41:14 -0500

    Time: 158 ms (81.06%), Space: 24.7 MB (58.81%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -8,8 +8,7 @@ class Solution:
             counts[num + offset] += 1 # shifts the range from [-50k, 50k] to [0, 100k] for array indexing
         index = 0
         for i in range(count_size):
-            count = counts[i]
-            for _ in range(count):
+            for _ in range(counts[i]):
                 nums[index] = i - offset # shifts back from [0, 100k] to [-50k, 50k] for the actual values
                 index += 1
         return nums
