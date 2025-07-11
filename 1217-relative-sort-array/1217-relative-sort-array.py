fb1d441c82895d24fb73edca6b56bf3c3bf1af36
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-11 10:06:53 -0500

    Time: 3 ms (43.21%), Space: 17.9 MB (68.24%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,14 +1,14 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        size = 1_000 + 1
-        buckets = [0] * size
+        size = 1_000 + 1 # constraint says numbers are 0-1000
+        counts = [0] * size
         for num in arr1:
-            buckets[num] += 1
+            counts[num] += 1
         result = []
         for num in arr2:
-            result.extend([num] * buckets[num])
-            buckets[num] = 0
-        for i, freq in enumerate(buckets):
+            result.extend([num] * counts[num])
+            counts[num] = 0 # mark used
+        for i, freq in enumerate(counts):
             if freq:
                 result.extend([i] * freq)
         return result
