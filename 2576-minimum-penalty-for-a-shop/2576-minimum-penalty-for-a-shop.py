a0f538ccf4b1ceba0a4076904207d66dfc0e5781
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-11 07:48:11 -0500

    Time: 59 ms (73.57%), Space: 18 MB (79.82%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -3,15 +3,13 @@ class Solution:
         left_sum, right_sum = 0, customers.count("Y")
         min_penalty = right_sum
         min_index = 0
-
         for i, state in enumerate(customers):
             if state == "Y":
                 right_sum -= 1
             else:
                 left_sum += 1
-            
-            curr_diff = left_sum + right_sum
-            if curr_diff < min_penalty:
-                min_penalty = curr_diff
+            curr_penalty = left_sum + right_sum
+            if curr_penalty < min_penalty:
+                min_penalty = curr_penalty
                 min_index = i + 1
         return min_index
