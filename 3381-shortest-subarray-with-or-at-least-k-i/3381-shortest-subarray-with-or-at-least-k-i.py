755b6e23c102cf61877aa3a6f1c61c51694501dc
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-11 09:08:31 -0500

    Time: 16 ms (19.33%), Space: 17.8 MB (60.59%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
@@ -1,29 +1,29 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        left = right = 0
-        shortest = float('inf')
-        NUM_BITS = 6
+        shortest = math.inf
+        NUM_BITS = 8
         bits = [0] * NUM_BITS
-
-        def add() -> None:
-            for i in range(NUM_BITS):
-                if nums[right] >> i & 1:
+        left = right = 0
+        def add():
+            for i in reversed(range(NUM_BITS)):
+                if (nums[right] >> i) & 1:
                     bits[i] += 1
-        def remove() -> None:
-            for i in range(NUM_BITS):
-                if nums[left] >> i & 1:
+        def remove():
+            for i in reversed(range(NUM_BITS)):
+                if (nums[left] >> i) & 1:
                     bits[i] -= 1
-        def total_or() -> int:
+        def at_least_k():
             total = 0
             for i in range(NUM_BITS):
                 if bits[i]:
-                    total |= 1 << i
-            return total
+                    total += 1 << i
+            return total >= k
+
         while right < len(nums):
             add()
-            while left <= right and total_or() >= k:
+            while left <= right and at_least_k():
                 shortest = min(shortest, right - left + 1)
                 remove()
                 left += 1
             right += 1
-        return -1 if shortest == float('inf') else shortest
+        return -1 if shortest == math.inf else shortest
