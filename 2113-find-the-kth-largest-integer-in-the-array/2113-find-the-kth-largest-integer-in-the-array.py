4ec7818c3c37675dc286db385aa2de19580be5da
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-11 10:39:07 -0500

    Time: 18 ms (94.67%), Space: 25.4 MB (56.43%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
@@ -1,21 +1,9 @@
 class Solution:
-    def kthLargestNumber(self, inums: List[str], k: int) -> str:
-        inums = [int(x) for x in inums]
-        k = len(inums) - k
-        def quick_select(l, r) -> int:
-            pivot_index = random.randint(l, r)
-            inums[r], inums[pivot_index] = inums[pivot_index], inums[r]
-            pivot = inums[r]
-            p = l
-            for i in range(l, r):
-                if inums[i] <= pivot:
-                    inums[i], inums[p] = inums[p], inums[i]
-                    p += 1
-            inums[p], inums[r] = inums[r], inums[p]
-            if p > k:
-                return quick_select(l, p - 1)
-            elif p < k:
-                return quick_select(p + 1, r)
-            else:
-                return inums[p]
-        return str(quick_select(0, len(inums) - 1))
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        min_heap = []
+        for i in range(k):
+            min_heap.append(int(nums[i]))
+        heapq.heapify(min_heap)
+        for num in nums[k:]:
+            heapq.heappushpop(min_heap, int(num))
+        return str(min_heap[0])
