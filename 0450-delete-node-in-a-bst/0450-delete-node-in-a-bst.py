9d0fe405d0fd454c6d3a26e2c9bc42825cd4e439
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-11 07:23:43 -0500

    Time: 6 ms (2.9%), Space: 21.4 MB (30.89%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -8,11 +8,9 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
-        if root.val > key:
-            root.left = self.deleteNode(root.left, key)
-        elif root.val < key:
-            root.right = self.deleteNode(root.right, key)
-        else:
+        root.left = self.deleteNode(root.left, key)
+        root.right = self.deleteNode(root.right, key)
+        if root.val == key:
             if not root.left:
                 return root.right
             if not root.right:
@@ -21,5 +19,5 @@ class Solution:
             while successor.left:
                 successor = successor.left
             root.val = successor.val
-            root.right = self.deleteNode(root.right, root.val)
+            root.right = self.deleteNode(root.right, root.val)    
         return root
