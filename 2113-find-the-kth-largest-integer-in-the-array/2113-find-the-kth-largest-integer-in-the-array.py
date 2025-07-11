e444d7716d1edb0ca7c3f414afd560e8185ac4ba
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-11 10:53:50 -0500

    Time: 6920 ms (5.02%), Space: 29.4 MB (12.43%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
@@ -1,21 +1,21 @@
 class Solution:
-    def kthLargestNumber(self, inums: List[str], k: int) -> str:
-        inums = [int(x) for x in inums]
-        k = len(inums) - k
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        inums = [int(num) for num in nums]
+        k = len(nums) - k
         def quick_select(l, r) -> int:
+            p = l
             pivot_index = random.randint(l, r)
             inums[r], inums[pivot_index] = inums[pivot_index], inums[r]
             pivot = inums[r]
-            p = l
             for i in range(l, r):
                 if inums[i] <= pivot:
                     inums[i], inums[p] = inums[p], inums[i]
                     p += 1
-            inums[p], inums[r] = inums[r], inums[p]
-            if p > k:
-                return quick_select(l, p - 1)
-            elif p < k:
+            inums[r], inums[p] = inums[p], inums[r]
+            if p < k :
                 return quick_select(p + 1, r)
+            elif p > k:
+                return quick_select(l, p - 1)
             else:
                 return inums[p]
         return str(quick_select(0, len(inums) - 1))
