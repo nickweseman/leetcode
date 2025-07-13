3adf53744435bf6dcd079c790964fe8cb34f4608
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 14:43:02 -0500

    Time: 0 ms (100%), Space: 18.3 MB (49.46%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -1,16 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # Create a timeline of all possible locations (0 to 1000).
-        # The size is 1001 to include location 1000.
-        timeline = [0] * 1_001
-        # Record the change in passengers at each pickup and drop-off location.
+        trips.sort(key=lambda x: x[1])
+        min_heap = []
+        time = 0
         for num_passengers, start, end in trips:
-            timeline[start] += num_passengers
-            timeline[end] -= num_passengers
-        # Iterate through the timeline to check the number of passengers at each point.
-        current_passengers = 0
-        for passenger_change in timeline:
-            current_passengers += passenger_change
-            if current_passengers > capacity:
+            time = start
+            while min_heap and min_heap[0][0] <= time:
+                _, dropped_off_passengers = heapq.heappop(min_heap)
+                capacity += dropped_off_passengers
+            if num_passengers > capacity:
                 return False
+            capacity -= num_passengers
+            heapq.heappush(min_heap, (end, num_passengers))
         return True
