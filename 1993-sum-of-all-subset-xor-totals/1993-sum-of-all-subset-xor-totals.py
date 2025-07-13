52411bc8ecf973184846bcc801f4fbc79d8f7e4c
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 16:03:47 -0500

    Time: 15 ms (47.23%), Space: 17.6 MB (97.03%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,15 +1,12 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         total_xor_sum = 0
-        comb = []
         def dfs(i, current_xor):
             nonlocal total_xor_sum
             if i == len(nums):
                 total_xor_sum += current_xor
                 return
-            comb.append(nums[i])
             dfs(i + 1, current_xor ^ nums[i])
-            comb.pop()
             dfs(i + 1, current_xor)
         dfs(0, 0)
         return total_xor_sum
