a2968318effdd2c26165e5fa60a617338e7bcc7c
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 08:57:46 -0500

    Time: 8 ms (30.37%), Space: 22.3 MB (10.74%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -11,13 +11,19 @@
 #         self.right = right
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        arr = []
+        if not head:
+            return None
+        values = []
         while head:
-            arr.append(head.val)
+            values.append(head.val)
             head = head.next
-        def create_tree(left, right) -> Optional[TreeNode]:
+        def dfs(left, right):
             if left > right:
                 return None
             mid = (left + right) // 2
-            return TreeNode(val=arr[mid], left=create_tree(left, mid - 1), right=create_tree(mid + 1, right))
-        return create_tree(0, len(arr) - 1)
+            node = TreeNode(values[mid])
+            node.left = dfs(left, mid - 1)
+            node.right = dfs(mid + 1, right)
+            return node
+        return dfs(0, len(values) - 1)
+            
