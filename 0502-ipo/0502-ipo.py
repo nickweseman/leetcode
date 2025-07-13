0d705397c3b5f44c1c519f55594f4258c5db8973
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 13:17:20 -0500

    Time: 409 ms (33.13%), Space: 42.1 MB (82.26%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -3,19 +3,15 @@ class Solution:
         project_min_heap = []
         profit_max_heap = []
         n = len(capital)
-        num_projects = 0
         for i in range(n):
             project_min_heap.append((capital[i], profits[i]))
         heapq.heapify(project_min_heap)
-        while project_min_heap or profit_max_heap:
+        for _ in range(k):
             while project_min_heap and project_min_heap[0][0] <= w:
                 needed_capital, profit = heapq.heappop(project_min_heap)
                 heapq.heappush(profit_max_heap, -profit)
             if not profit_max_heap:
                 break
             w += -heapq.heappop(profit_max_heap)
-            num_projects += 1
-            if num_projects == k:
-                break
         return w
             
