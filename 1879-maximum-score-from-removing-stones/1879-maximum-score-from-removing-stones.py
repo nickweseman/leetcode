68b406701d01f62121836d4c39736f038fb0091e
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 09:35:41 -0500

    Time: 0 ms (100%), Space: 17.5 MB (97.31%) - LeetHub

--- 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.py
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.py
@@ -1,22 +1,7 @@
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
-        score = 0
-        max_heap = []
-        if a > 0:
-            max_heap.append(-a)
-        if b > 0:
-            max_heap.append(-b)
-        if c > 0:
-            max_heap.append(-c)
-        heapq.heapify(max_heap)
-        while len(max_heap) > 1:
-            pile1 = -heapq.heappop(max_heap)
-            pile2 = -heapq.heappop(max_heap)
-            score += 1
-            pile1 -= 1
-            pile2 -= 1
-            if pile1 > 0:
-                heapq.heappush(max_heap, -pile1)
-            if pile2 > 0:
-                heapq.heappush(max_heap, -pile2)
-        return score
+        small, med, large = sorted([a, b, c])
+        if small + med < large:
+            return small + med
+        else:
+            return (small + med + large) // 2
