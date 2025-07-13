ffbbce4ac9b7a09121c54f20e21af1bb2eac414c
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 14:18:16 -0500

    Time: 0 ms (100%), Space: 17.7 MB (94.06%) - LeetHub

--- 2327-largest-number-after-digit-swaps-by-parity/2327-largest-number-after-digit-swaps-by-parity.py
+++ 2327-largest-number-after-digit-swaps-by-parity/2327-largest-number-after-digit-swaps-by-parity.py
@@ -1,23 +1,23 @@
 class Solution:
     def largestInteger(self, num: int) -> int:
-        num_str = str(num)
+        s = str(num)
         odd_max_heap = []
         even_max_heap = []
-        for d in num_str:
+        for d in s:
             digit = int(d)
-            if digit % 2 == 0:
-                even_max_heap.append(-digit)
-            else:
+            if digit & 1 == 1:
                 odd_max_heap.append(-digit)
-        heapq.heapify(even_max_heap)
-        heapq.heapify(odd_max_heap)
-        result = []
-        for d in num_str:
-            digit = int(d)
-            if digit % 2 == 0:
-                largest_even = -heapq.heappop(even_max_heap)
-                result.append(str(largest_even))
             else:
-                largest_odd = -heapq.heappop(odd_max_heap)
-                result.append(str(largest_odd))
+                even_max_heap.append(-digit)
+        heapq.heapify(odd_max_heap)
+        heapq.heapify(even_max_heap)
+        result = []
+        for d in s:
+            digit = int(d)
+            if digit & 1 == 1:
+                result.append(str(-heapq.heappop(odd_max_heap)))
+            else:
+                result.append(str(-heapq.heappop(even_max_heap)))
         return int("".join(result))
+            
+
