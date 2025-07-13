6ddea5824ef232176d2b284e951abbbbb3d72548
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 07:23:13 -0500

    Time: 0 ms (100%), Space: 20 MB (10.68%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
@@ -6,14 +6,14 @@
 #         self.right = right
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        total = 0
-        def dfs(node) -> None:
-            nonlocal total
-            if node:
-                dfs(node.right)
-                total += node.val
-                node.val = total
-                dfs(node.left)
+        rolling_sum = 0
+        def dfs(node):
+            nonlocal rolling_sum
+            if not node:
+                return
+            dfs(node.right)
+            rolling_sum += node.val
+            node.val = rolling_sum
+            dfs(node.left)
         dfs(root)
         return root
-        
