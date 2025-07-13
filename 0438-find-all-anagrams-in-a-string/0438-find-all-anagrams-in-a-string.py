dac26c3fb4c5482aa913c229b0a7a29bce1b7cf5
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-13 08:33:08 -0500

    Time: 51 ms (56.33%), Space: 18.3 MB (56.87%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,18 +1,15 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         result = []
-        target = collections.Counter(p)
-        window = collections.Counter()
         left = right = 0
-
+        window = collections.defaultdict(int)
+        target = collections.Counter(p)
         have = 0
         need = len(target)
-
         while right < len(s):
             window[s[right]] += 1
             if window[s[right]] == target[s[right]]:
                 have += 1
-            
             if right - left + 1 > len(p):
                 if window[s[left]] == target[s[left]]:
                     have -= 1
@@ -22,3 +19,4 @@ class Solution:
                 result.append(left)
             right += 1
         return result
+             
