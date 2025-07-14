a460ee186bcda205579ecba685b7b0807f4c0020
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-14 13:17:45 -0500

    Time: 47 ms (94.24%), Space: 17.9 MB (70.5%) - LeetHub

---
+++ 2318-maximum-points-in-an-archery-competition/2318-maximum-points-in-an-archery-competition.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumBobPoints(self, numArrows: int, aliceArrows: List[int]) -> List[int]:
+        bob_best_arrows = [0] * 12
+        comb = [0] * 12
+        max_score = -math.inf
+        def dfs(i, arrows_left, current_score):
+            nonlocal max_score, bob_best_arrows
+            if i < 1:
+                if current_score > max_score:
+                    max_score = current_score
+                    comb[0] = arrows_left # dump remaining arrows in 0 to spend them all
+                    bob_best_arrows = comb.copy()
+                return
+            cost = aliceArrows[i] + 1
+            if arrows_left >= cost:
+                comb[i] = cost
+                dfs(i - 1, arrows_left - cost, current_score + i)
+                comb[i] = 0
+            dfs(i - 1, arrows_left, current_score)
+        dfs(11, numArrows, 0)
+        return bob_best_arrows
+        
