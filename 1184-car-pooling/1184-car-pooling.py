5be88ce038715f45eee1acf4d023fd22844f2684
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-16 07:00:55 -0500

    Time: 0 ms (100%), Space: 18.3 MB (28.57%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -1,12 +1,11 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        curr_passengers = [0] * 1_001
+        passengers = [0] * 1_001
         for num_passengers, start, end in trips:
-            curr_passengers[start] -= num_passengers
-            curr_passengers[end] += num_passengers
-        simulated_passengers = capacity
-        for i in range(len(curr_passengers)):
-            simulated_passengers += curr_passengers[i]
-            if simulated_passengers < 0:
+            passengers[start] -= num_passengers
+            passengers[end] += num_passengers
+        for i, num in enumerate(passengers):
+            capacity += num
+            if capacity < 0:
                 return False
         return True
