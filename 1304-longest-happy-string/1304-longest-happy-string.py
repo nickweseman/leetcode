d1d868fc52bba60c878496701314270b19f894bb
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-16 13:22:33 -0500

    Time: 0 ms (100%), Space: 18 MB (23.71%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -10,25 +10,19 @@ class Solution:
         heapq.heapify(max_heap)
         result = []
         while max_heap:
-            ch = max_heap[0][1]
-            if result[-2:] == [ch, ch]:
-                print("got here")
-                neg_count1, letter1 = heapq.heappop(max_heap)
-                count1 = -neg_count1
+            neg_char1, c1 = heapq.heappop(max_heap)
+            char1 = -neg_char1
+            if result[-2:] == [c1, c1]:
                 if not max_heap:
                     break
-                neg_count2, letter2 = heapq.heappop(max_heap)
-                count2 = -neg_count2
-                count2 -= 1
-                result.append(letter2)
-                if count2 > 0:
-                    heapq.heappush(max_heap, (-count2, letter2))
-                heapq.heappush(max_heap, (-count1, letter1))
-            else:
-                neg_count, letter = heapq.heappop(max_heap)
-                count = -neg_count
-                count -= 1
-                result.append(letter)
-                if count > 0:
-                    heapq.heappush(max_heap, (-count, letter))
+                neg_char2, c2 = heapq.heappop(max_heap)
+                char2 = -neg_char2
+                char2 -= 1
+                result.append(c2)
+                if char2 > 0:
+                    heapq.heappush(max_heap, (-char2, c2))
+            result.append(c1)
+            char1 -= 1
+            if char1 > 0:
+                heapq.heappush(max_heap, (-char1, c1))   
         return "".join(result)
