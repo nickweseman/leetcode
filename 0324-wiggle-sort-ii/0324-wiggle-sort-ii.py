68efe2d4a32f58f201332c7d6309f5c9335be21b
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-16 06:25:34 -0500

    Time: 2094 ms (5.12%), Space: 20.9 MB (5.87%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -2,37 +2,36 @@ class Solution:
     def wiggleSort(self, nums: List[int]) -> None:
         n = len(nums)
         k = n // 2
-        def quick_select(l, r) -> int:
-            p = l
-            pivot_index = random.randint(l, r)
-            nums[r], nums[pivot_index] = nums[pivot_index], nums[r]
+        def get_median(l, r):
             pivot = nums[r]
+            p = l
             for i in range(l, r):
-                if nums[i] <= pivot:
+                if nums[i] < pivot:
                     nums[i], nums[p] = nums[p], nums[i]
                     p += 1
-            nums[p], nums[r] = nums[r], nums[p]
-            if p > k:
-                return quick_select(l, p - 1)
-            elif p < k:
-                return quick_select(p + 1, r)
+            nums[r], nums[p] = nums[p], nums[r]
+            if p < k:
+                return get_median(p + 1, r)
+            elif p > k:
+                return get_median(l, p - 1)
             else:
                 return nums[p]
-        median = quick_select(0, n - 1)
-        def map_index(i) -> int:
+        median = get_median(0, n - 1)
+        def map_index(i):
             return (2 * i + 1) % (n | 1)
         low, mid, high = 0, 0, n - 1
         while mid <= high:
-            map_mid = map_index(mid)
-            if nums[map_mid] > median:
-                map_low = map_index(low)
-                nums[map_mid], nums[map_low] = nums[map_low], nums[map_mid]
+            mapped_mid = map_index(mid)
+            if nums[mapped_mid] > median:
+                mapped_low = map_index(low)
+                nums[mapped_low], nums[mapped_mid] = nums[mapped_mid], nums[mapped_low]
                 low += 1
                 mid += 1
-            elif nums[map_mid] == median:
-                mid += 1
+            elif nums[mapped_mid] < median:
+                mapped_high = map_index(high)
+                nums[mapped_high], nums[mapped_mid] = nums[mapped_mid], nums[mapped_high]
+                high -= 1
             else:
-                map_high = map_index(high)
-                nums[map_mid], nums[map_high] = nums[map_high], nums[map_mid]
-                high -= 1 
+                mid += 1
+                
         
