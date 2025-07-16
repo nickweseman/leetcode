579d4c0f78145a19dedd620f740677be55b906a4
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-16 17:17:08 -0500

    Time: 2 ms (38.25%), Space: 19.1 MB (65.27%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
@@ -8,19 +8,16 @@ class Solution:
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
         if not root or depth == 1:
             return TreeNode(val, left=root)
-        queue = collections.deque([(root, 1)]) # node, curr_depth
+        queue = collections.deque([(root, 1)])
         while queue:
             for _ in range(len(queue)):
-                curr, curr_depth = queue.popleft()
-                if curr_depth == depth - 1:
+                curr, height = queue.popleft()
+                if height == depth - 1:
                     curr.left = TreeNode(val, left=curr.left)
                     curr.right = TreeNode(val, right=curr.right)
                 else:
                     if curr.left:
-                        queue.append((curr.left, curr_depth + 1))
+                        queue.append((curr.left, height + 1))
                     if curr.right:
-                        queue.append((curr.right, curr_depth + 1))
+                        queue.append((curr.right, height + 1))
         return root
-        
-        
-        
