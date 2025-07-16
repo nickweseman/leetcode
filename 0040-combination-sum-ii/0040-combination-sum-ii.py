1d80c29f7b639d7dde1490042d55c064758015eb
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-16 11:59:49 -0500

    Time: 31 ms (24.9%), Space: 18 MB (34.84%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,21 +1,20 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        comb = []
+        path = []
         candidates.sort()
-        used = [False] * len(candidates)
         def dfs(index, remaining_target):
             if remaining_target == 0:
-                result.append(comb.copy())
+                result.append(path.copy())
                 return
             if index == len(candidates) or remaining_target < 0:
                 return
-            if index == 0 or candidates[index] != candidates[index - 1] or used[index - 1]:
-                comb.append(candidates[index])
-                used[index] = True
-                dfs(index + 1, remaining_target - candidates[index])
-                comb.pop()
-                used[index] = False
-            dfs(index + 1, remaining_target)
+            path.append(candidates[index])
+            dfs(index + 1, remaining_target - candidates[index])
+            path.pop()
+            next_i = index + 1
+            while next_i < len(candidates) and candidates[next_i] == candidates[next_i - 1]:
+                next_i += 1
+            dfs(next_i, remaining_target)
         dfs(0, target)
         return result
