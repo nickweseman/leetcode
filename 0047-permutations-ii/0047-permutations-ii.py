5ccc3e3607d1b0e02e463c85f909d103bd1d3f52
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 11:33:38 -0500

    Time: 3 ms (94.06%), Space: 18.1 MB (53.72%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -10,11 +10,14 @@ class Solution:
                 result.append(path.copy())
                 return
             for i in range(n):
-                if (i == 0 or nums[i] != nums[i - 1] or used[i - 1]) and not used[i]:
-                    path.append(nums[i])
-                    used[i] = True
-                    backtrack(index + 1)
-                    used[i] = False
-                    path.pop()
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+                path.append(nums[i])
+                used[i] = True
+                backtrack(index + 1)
+                used[i] = False
+                path.pop()
         backtrack(0)
         return result
