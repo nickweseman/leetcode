dd8b59879f80946a0a1504d2551153718bac606b
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 08:03:24 -0500

    Time: 0 ms (100%), Space: 17.7 MB (51.17%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         left, right = 0, num
-
         while left <= right:
             mid = (left + right) // 2
             if mid * mid == num:
