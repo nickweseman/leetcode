39a4e7beca8500193169dfd8de6082ab7ec9ef5b
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 18:48:28 -0500

    Time: 4728 ms (15.63%), Space: 17.8 MB (79.91%) - LeetHub

---
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProduct(self, s: str) -> int:
+        max_product = 0
+        s1 = []
+        s2 = []
+        def backtrack(index):
+            nonlocal max_product
+            if index == len(s):
+                if s1 == s1[::-1] and s2 == s2[::-1]:
+                    max_product = max(max_product, len(s1) * len(s2))
+                return
+            s1.append(s[index])
+            backtrack(index + 1)
+            s1.pop()
+            s2.append(s[index])
+            backtrack(index + 1)
+            s2.pop()
+            backtrack(index + 1)
+        backtrack(0)
+        return max_product
