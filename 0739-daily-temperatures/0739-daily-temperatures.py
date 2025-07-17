c87922fdbca390368dfb62d679c0303425f1c64e
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-16 19:21:15 -0500

    Time: 82 ms (91.31%), Space: 27.6 MB (53.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,7 +2,6 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         answer = [0] * len(temperatures)
         stack = []
-
         for i, num in enumerate(temperatures):
             while stack and temperatures[stack[-1]] < num:
                 idx = stack.pop()
