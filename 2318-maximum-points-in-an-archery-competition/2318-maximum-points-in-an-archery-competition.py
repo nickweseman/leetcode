d66bd5defdfb4663d1711fd0c73e70ddd9aa1e4e
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 17:41:29 -0500

    Time: 55 ms (88.16%), Space: 18 MB (28.29%) - LeetHub

--- 2318-maximum-points-in-an-archery-competition/2318-maximum-points-in-an-archery-competition.py
+++ 2318-maximum-points-in-an-archery-competition/2318-maximum-points-in-an-archery-competition.py
@@ -1,22 +1,23 @@
 class Solution:
     def maximumBobPoints(self, numArrows: int, aliceArrows: List[int]) -> List[int]:
-        bob_best_arrows = [0] * 12
-        bob_current_arrows = [0] * 12
-        max_score = -math.inf
-        def dfs(i, arrows_left, current_score):
-            nonlocal max_score, bob_best_arrows
-            if i < 1:
-                if current_score > max_score:
-                    max_score = current_score
-                    bob_current_arrows[0] = arrows_left # dump remaining arrows in 0 to spend them all
-                    bob_best_arrows = bob_current_arrows.copy()
+        best_bob_arrows = [0] * 12
+        current_bob_arrows = [0] * 12
+        max_score = 0
+        def backtrack(index, arrows_left, score_so_far):
+            nonlocal best_bob_arrows, max_score
+            if index == 0 or arrows_left == 0:
+                current_bob_arrows[0] = arrows_left
+                if score_so_far > max_score:
+                    max_score = score_so_far
+                    best_bob_arrows = current_bob_arrows.copy()
                 return
-            cost = aliceArrows[i] + 1
-            if arrows_left >= cost:
-                bob_current_arrows[i] = cost
-                dfs(i - 1, arrows_left - cost, current_score + i)
-                bob_current_arrows[i] = 0
-            dfs(i - 1, arrows_left, current_score)
-        dfs(11, numArrows, 0)
-        return bob_best_arrows
-        
+            if arrows_left < 0:
+                return
+            cost = aliceArrows[index] + 1
+            if cost <= arrows_left:
+                current_bob_arrows[index] = cost
+                backtrack(index - 1, arrows_left - cost, score_so_far + index)
+                current_bob_arrows[index] = 0
+            backtrack(index - 1, arrows_left, score_so_far)
+        backtrack(11, numArrows, 0)
+        return best_bob_arrows
