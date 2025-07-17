838503292a0d9582d29d2ab838e3cbedeff979c0
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 06:02:54 -0500

    Time: 0 ms (100%), Space: 17.9 MB (23.49%) - LeetHub

--- 2602-maximum-enemy-forts-that-can-be-captured/2602-maximum-enemy-forts-that-can-be-captured.py
+++ 2602-maximum-enemy-forts-that-can-be-captured/2602-maximum-enemy-forts-that-can-be-captured.py
@@ -1,13 +1,10 @@
 class Solution:
     def captureForts(self, forts: List[int]) -> int:
-        current_index = 0
-        max_zeroes = 0
-        last_fort_index = -1
-
-        while current_index < len(forts):
-            if forts[current_index] in (1, -1):
-                if last_fort_index != -1 and forts[last_fort_index] * forts[current_index] == -1:
-                    max_zeroes = max(max_zeroes, current_index - last_fort_index - 1)
-                last_fort_index = current_index
-            current_index += 1
-        return max_zeroes
+        last_fort_index = None
+        max_captured = 0
+        for i, num in enumerate(forts):
+            if num in (1, -1):
+                if last_fort_index is not None and forts[last_fort_index] * num == -1:
+                    max_captured = max(max_captured, i - last_fort_index - 1)
+                last_fort_index = i
+        return max_captured
