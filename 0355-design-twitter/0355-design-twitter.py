80eb6dbde82edfd18dee4a11334cb62f1f605998
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 09:12:23 -0500

    Time: 19 ms (37.13%), Space: 27.7 MB (54.67%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,27 +1,28 @@
 class Twitter:
+
     def __init__(self):
-        self.time = 0
         self.follow_map = collections.defaultdict(set)
-        self.user_tweets = collections.defaultdict(collections.deque) # deque + appendleft makes sure that most recent tweet is always at index 0.
+        self.tweet_map = collections.defaultdict(collections.deque)
+        self.time = 0
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.user_tweets[userId].appendleft((self.time, tweetId))
+        self.tweet_map[userId].appendleft((self.time, tweetId))
         self.time += 1
     def getNewsFeed(self, userId: int) -> List[int]:
-        users = self.follow_map[userId].copy()
-        users.add(userId) # you get to see your own tweets without following yourself
         max_heap = []
-        for user in users:
-            if self.user_tweets[user]:
-                timestamp, tweet_id = self.user_tweets[user][0]
-                max_heap.append((-timestamp, tweet_id, user, 0)) # 0 is tweet_idx
+        news_followers = self.follow_map[userId].copy()
+        news_followers.add(userId)
+        for user in news_followers:
+            if self.tweet_map[user]:
+                time, tweet_id = self.tweet_map[user][0]
+                max_heap.append((-time, user, tweet_id, 0))
         heapq.heapify(max_heap)
         result = []
         while max_heap and len(result) < 10:
-            _, tweet_id, user, tweet_idx = heapq.heappop(max_heap)
+            _, user, tweet_id, tweet_idx = heapq.heappop(max_heap)
             result.append(tweet_id)
-            if tweet_idx + 1 < len(self.user_tweets[user]):
-                next_timestamp, next_tweet_id = self.user_tweets[user][tweet_idx + 1]
-                heapq.heappush(max_heap, (-next_timestamp, next_tweet_id, user, tweet_idx + 1))
+            if tweet_idx + 1 < len(self.tweet_map[user]):
+                time, tweet_id = self.tweet_map[user][tweet_idx + 1]
+                heapq.heappush(max_heap, (-time, user, tweet_id, tweet_idx + 1))
         return result
     def follow(self, followerId: int, followeeId: int) -> None:
         self.follow_map[followerId].add(followeeId)
