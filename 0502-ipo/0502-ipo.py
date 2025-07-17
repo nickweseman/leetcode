006aa542a14695d805b6865b3809257ff418cd04
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 07:27:05 -0500

    Time: 454 ms (22.22%), Space: 42.1 MB (70.93%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,14 +1,19 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        profit_max_heap = []
-        project_min_heap = list(zip(capital, profits))
-        heapq.heapify(project_min_heap)
-        for _ in range(k):
-            while project_min_heap and project_min_heap[0][0] <= w:
-                _, profit = heapq.heappop(project_min_heap)
-                heapq.heappush(profit_max_heap, -profit)
-            if not profit_max_heap:
+        projects_min_heap = []
+        for capital, profit in zip(capital, profits):
+            projects_min_heap.append((capital, profit))
+        heapq.heapify(projects_min_heap)
+        profits_max_heap = []
+        while k:
+            while projects_min_heap and projects_min_heap[0][0] <= w:
+                _, profit = heapq.heappop(projects_min_heap)
+                heapq.heappush(profits_max_heap, -profit)
+            if not profits_max_heap:
                 break
-            w += -heapq.heappop(profit_max_heap)
+            profit = -heapq.heappop(profits_max_heap)
+            w += profit
+            k -= 1
         return w
+
             
