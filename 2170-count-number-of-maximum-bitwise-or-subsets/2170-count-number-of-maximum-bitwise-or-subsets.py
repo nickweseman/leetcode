333fd12bedb5fa2a9ee4c6335c4f334b35bd28a7
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-17 19:26:17 -0500

    Time: 239 ms (66.04%), Space: 17.8 MB (57.92%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,22 +1,18 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         max_or = -math.inf
-        max_subsets = -1 #= []
+        max_subsets = -1
         path = []
         def backtrack(index, or_so_far):
             nonlocal max_or, max_subsets
             if index == len(nums):
-                
                 if or_so_far > max_or:
                     max_or = or_so_far
-                    max_subsets = 0 #[path.copy()]
+                    max_subsets = 0 
                 if or_so_far == max_or:
-                    max_subsets += 1 #max_subsets.append(path.copy)
-                #print(f"{path=}{or_so_far=}{max_or=}{max_subsets=}")
+                    max_subsets += 1
                 return
-            path.append(nums[index])
             backtrack(index + 1, or_so_far | nums[index])
-            path.pop()
             backtrack(index + 1, or_so_far)
         backtrack(0, 0)
         return max_subsets
