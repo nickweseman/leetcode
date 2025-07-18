d2861d1cfe7126f54898c5099df54fe94bbb15eb
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-18 11:18:17 -0500

    Time: 0 ms (100%), Space: 17.9 MB (57.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -10,10 +10,8 @@ class Solution:
             path.append(nums[index])
             backtrack(index + 1)
             path.pop()
-            next_index = index + 1
-            while next_index < len(nums) and nums[next_index] == nums[next_index - 1]:
-                next_index += 1
-            print(f"{index=}{next_index=}")
-            backtrack(next_index)
+            while index + 1 < len(nums) and nums[index + 1] == nums[index]:
+                index += 1
+            backtrack(index + 1)
         backtrack(0)
         return result
