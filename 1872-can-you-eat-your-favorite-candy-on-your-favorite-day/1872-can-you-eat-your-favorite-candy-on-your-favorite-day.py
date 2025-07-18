52b91ee105567214f202e7ab076cf9910b50aba6
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-18 10:34:45 -0500

    Time: 42 ms (69.03%), Space: 68.2 MB (5.81%) - LeetHub

--- 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
+++ 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.py
@@ -1,21 +1,14 @@
 class Solution:
     def canEat(self, candiesCount: List[int], queries: List[List[int]]) -> List[bool]:
-        n = len(candiesCount)
+        prefix_sum = [0]
+        for num in candiesCount:
+            prefix_sum.append(prefix_sum[-1] + num)
         result = []
-
-        for i in range(1, n):
-            candiesCount[i] += candiesCount[i - 1]
-        
         for favorite_type, favorite_day, daily_cap in queries:
             min_eaten = favorite_day + 1
-            max_eaten = (favorite_day + 1) * daily_cap
-
-            if favorite_type == 0:
-                first_favorite = 1
-            else:
-                first_favorite = candiesCount[favorite_type - 1] + 1
-            last_favorite = candiesCount[favorite_type]
-
+            max_eaten = daily_cap * (favorite_day + 1)
+            first_favorite = prefix_sum[favorite_type] + 1
+            last_favorite = prefix_sum[favorite_type + 1]
             can_reach = min_eaten <= last_favorite and max_eaten >= first_favorite
             result.append(can_reach)
         return result
