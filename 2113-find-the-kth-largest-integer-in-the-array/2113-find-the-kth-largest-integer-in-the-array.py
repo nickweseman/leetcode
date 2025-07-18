c75cf0def23433caa0d66c9ed1e20ad07a6574c8
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-18 09:38:41 -0500

    Time: 5116 ms (5.07%), Space: 28.8 MB (12.51%) - LeetHub

--- 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/2113-find-the-kth-largest-integer-in-the-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums = [int(num) for num in nums]
         n = len(nums)
         k = n - k
-        nums = [int(num) for num in nums]
         def quick_select(l, r):
             pivot_index = random.randint(l, r)
             nums[r], nums[pivot_index] = nums[pivot_index], nums[r]
@@ -12,11 +12,11 @@ class Solution:
                 if nums[i] < pivot:
                     nums[i], nums[p] = nums[p], nums[i]
                     p += 1
-            nums[p], nums[r] = nums[r], nums[p]
-            if p < k:
-                return quick_select(p + 1, r)
-            elif p > k:
+            nums[r], nums[p] = nums[p], nums[r]
+            if p > k:
                 return quick_select(l, p - 1)
+            elif p < k:
+                return quick_select(p + 1, r)
             else:
                 return nums[p]
         return str(quick_select(0, n - 1))
