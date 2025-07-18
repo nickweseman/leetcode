017808f5ecf6d5291ba5b68d841b347eff811863
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-18 13:30:25 -0500

    Time: 0 ms (100%), Space: 17.9 MB (27.91%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,20 +1,18 @@
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        LEDS = [8, 4, 2, 1, 32, 16, 8, 4, 2, 1]
         result = []
-        LEDS = [8,4,2,1,32,16,8,4,2,1]
-        n = len(LEDS)
-        def dfs(i, leds_left, hour, minute):
-            if hour > 11 or minute > 59:
+        def backtrack(index, turned_on_left, hour, minute):
+            if hour > 11 or minute > 59 or turned_on_left < 0:
                 return
-            if leds_left == 0:
-                result.append(f"{hour}:{minute:02d}")
+            if index == len(LEDS):
+                if turned_on_left == 0:
+                    result.append(f"{hour}:{minute:02d}")
                 return
-            if i == n:
-                return
-            if i < 4:
-                dfs(i + 1, leds_left - 1, hour + LEDS[i], minute)
+            if index < 4:
+                backtrack(index + 1, turned_on_left - 1, hour + LEDS[index], minute)
             else:
-                dfs(i + 1, leds_left - 1, hour, minute + LEDS[i])
-            dfs(i + 1, leds_left, hour, minute)
-        dfs(0, turnedOn, 0, 0)
+                backtrack(index + 1, turned_on_left - 1, hour, minute + LEDS[index])
+            backtrack(index + 1, turned_on_left, hour, minute)
+        backtrack(0, turnedOn, 0, 0)
         return result
