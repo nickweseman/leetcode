b26829f9b2d848132a124b3ad3d41c4e43f16812
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-18 12:58:21 -0500

    Time: 5 ms (5.79%), Space: 19.8 MB (29.58%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -9,16 +9,15 @@ class Solution:
         # colorful, blue and green
         def dfs(node):
             if not node:
-                return (True, None, None)
+                return (True, math.inf, -math.inf)
             if not node.left and not node.right:
                 return (True, node.val, node.val)
             left_balanced, left_val_low, left_val_high = dfs(node.left)
             right_balanced, right_val_low, right_val_high = dfs(node.right)
             me_balanced = (left_balanced and right_balanced and 
-                (left_val_high is None or left_val_high < node.val) and 
-                (right_val_low is None or node.val < right_val_low))
-            me_val_low = min(left_val_low, node.val) if left_val_low else node.val
-            me_val_high = max(right_val_high, node.val) if right_val_high else node.val
+                (left_val_high < node.val) and 
+                (node.val < right_val_low))
+            me_val_low = min(left_val_low, node.val)
+            me_val_high = max(right_val_high, node.val)
             return (me_balanced, me_val_low, me_val_high)
         return dfs(root)[0]
-        
