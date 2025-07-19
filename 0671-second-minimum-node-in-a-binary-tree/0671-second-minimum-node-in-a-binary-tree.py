b476808f150b5b6954281a0f658864569d6450a0
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-19 11:28:41 -0500

    Time: 0 ms (100%), Space: 18 MB (10.49%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
@@ -6,16 +6,18 @@
 #         self.right = right
 class Solution:
     def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return -1
         min1 = root.val
         min2 = math.inf
-        def dfs(node) -> int:
-            nonlocal min2
-            if not node:
-                return math.inf
-            if node.val > min1:
-                min2 = min(min2, node.val)
-            left = dfs(node.left)
-            right = dfs(node.right)
-        dfs(root)
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            if curr.val > min1:
+                min2 = min(min2, curr.val)
+            curr = curr.right
         return -1 if min2 == math.inf else min2
-        
