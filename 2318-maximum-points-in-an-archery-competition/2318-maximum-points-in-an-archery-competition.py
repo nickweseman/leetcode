9fcd68daea50621cc0c1f80d677fe5d6bafbd9ba
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-20 17:30:53 -0500

    Time: 48 ms (91.84%), Space: 18.1 MB (26.53%) - LeetHub

--- 2318-maximum-points-in-an-archery-competition/2318-maximum-points-in-an-archery-competition.py
+++ 2318-maximum-points-in-an-archery-competition/2318-maximum-points-in-an-archery-competition.py
@@ -1,23 +1,23 @@
 class Solution:
     def maximumBobPoints(self, numArrows: int, aliceArrows: List[int]) -> List[int]:
-        best_bob_arrows = [0] * 12
+        best_bob_arrows = []
         current_bob_arrows = [0] * 12
-        max_score = 0
-        def backtrack(index, arrows_left, score_so_far):
-            nonlocal best_bob_arrows, max_score
-            if index == 0 or arrows_left == 0:
-                current_bob_arrows[0] = arrows_left
-                if score_so_far > max_score:
-                    max_score = score_so_far
+        max_score = -math.inf
+        def backtrack(index, arrows_left, score):
+            nonlocal max_score, best_bob_arrows
+            if index == 0:
+                if score > max_score:
+                    max_score = score
+                    if arrows_left:
+                        current_bob_arrows[0] = arrows_left
                     best_bob_arrows = current_bob_arrows.copy()
-                return
-            if arrows_left < 0:
+                    current_bob_arrows[0] = 0
                 return
             cost = aliceArrows[index] + 1
-            if cost <= arrows_left:
+            if arrows_left >= cost:
                 current_bob_arrows[index] = cost
-                backtrack(index - 1, arrows_left - cost, score_so_far + index)
+                backtrack(index - 1, arrows_left - cost, score + index)
                 current_bob_arrows[index] = 0
-            backtrack(index - 1, arrows_left, score_so_far)
+            backtrack(index - 1, arrows_left, score)
         backtrack(11, numArrows, 0)
         return best_bob_arrows
