0d0636e3a94f4300888faeeab200444b0b839ffd
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-20 10:57:51 -0500

    Time: 363 ms (36.63%), Space: 17.8 MB (78.39%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -6,13 +6,14 @@ class Solution:
         for i in range(n):
             left, right = i + 1, n - 1
             while left < right:
-                sum_ = nums[i] + nums[left] + nums[right]
-                if abs(sum_ - target) < abs(closest_sum - target):
-                    closest_sum = sum_
-                if sum_ == target:
-                    return sum_
-                elif sum_ < target:
+                test_sum = nums[i] + nums[left] + nums[right]
+                if abs(test_sum - target) < abs(closest_sum - target):
+                    closest_sum = test_sum
+                if test_sum == target:
+                    return test_sum
+                elif test_sum < target:
                     left += 1
                 else:
                     right -= 1
         return closest_sum
+                
