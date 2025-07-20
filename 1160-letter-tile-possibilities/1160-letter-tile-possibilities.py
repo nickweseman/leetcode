7f027bcd39e632bddc496a9c53c58912f5f1548f
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-20 07:08:56 -0500

    Time: 23 ms (86.55%), Space: 17.7 MB (84.45%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -1,20 +1,25 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        result = set()
+        num_possibilities = 0
         path = []
         n = len(tiles)
+        my_tiles = list(tiles)
+        my_tiles.sort()
         used = [False] * n
         def backtrack(index):
+            nonlocal num_possibilities
             if index == n:
                 return
             for i in range(n):
                 if used[i]:
                     continue
+                if i > 0 and my_tiles[i] == my_tiles[i - 1] and not used[i - 1]:
+                    continue
+                num_possibilities += 1
+                path.append(my_tiles[i])
                 used[i] = True
-                path.append(tiles[i])
-                result.add(tuple(path))
                 backtrack(index + 1)
                 path.pop()
                 used[i] = False
         backtrack(0)
-        return len(result)
+        return num_possibilities
