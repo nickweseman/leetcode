c0450bf2944b37f556999aaee0f589401d7f0a62
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-21 09:56:15 -0500

    Time: 15 ms (39.02%), Space: 17.9 MB (39.02%) - LeetHub

--- 2482-maximum-rows-covered-by-columns/2482-maximum-rows-covered-by-columns.py
+++ 2482-maximum-rows-covered-by-columns/2482-maximum-rows-covered-by-columns.py
@@ -1,30 +1,28 @@
 class Solution:
     def maximumRows(self, matrix: List[List[int]], numSelect: int) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
         max_rows = 0
-        num_cols = len(matrix[0])
-        num_rows = len(matrix)
-        cols_selected = [False] * num_cols
-        def get_covered_rows():
+        cols_selected = [False] * COLS
+        def get_covered_rows() -> int:
             count = 0
-            for i in range(num_rows):
-                row_covered = True
-                for j in range(num_cols):
-                    if matrix[i][j] == 1 and not cols_selected[j]:
-                        row_covered = False
-                if row_covered:
+            for r in range(ROWS):
+                covered = True
+                for c in range(COLS):
+                    if matrix[r][c] == 1 and not cols_selected[c]:
+                        covered = False
+                if covered:
                     count += 1
             return count
         def backtrack(index, cols_left):
             nonlocal max_rows
-            if cols_left < 0:
-                return
-            if index == num_cols:
+            if index == COLS:
                 if cols_left == 0:
                     max_rows = max(max_rows, get_covered_rows())
                 return
-            cols_selected[index] = True
-            backtrack(index + 1, cols_left - 1)
-            cols_selected[index] = False
+            if cols_left > 0:
+                cols_selected[index] = True
+                backtrack(index + 1, cols_left - 1)
+                cols_selected[index] = False
             backtrack(index + 1, cols_left)
         backtrack(0, numSelect)
         return max_rows
