b7ef3cfd69a483cb2d9ae46e9aa1c28f30695b9d
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-20 19:39:11 -0500

    Time: 23 ms (55.48%), Space: 24.8 MB (34.33%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,16 +1,17 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        result = set()
         path = []
+        result = set()
+        n = len(nums)
         def backtrack(index):
-            if index == len(nums):
+            if index == n: 
                 return
-            if not path or nums[index] >= path[-1]:
-                path.append(nums[index])
-                if len(path) > 1:
-                    result.add(tuple(path))
-                backtrack(index + 1)
-                path.pop()
-            backtrack(index + 1)
+            for i in range(index, n):
+                if not path or nums[i] >= path[-1]:
+                    path.append(nums[i])
+                    if len(path) >= 2:
+                        result.add(tuple(path))
+                    backtrack(i + 1)
+                    path.pop()
         backtrack(0)
         return list(result)
