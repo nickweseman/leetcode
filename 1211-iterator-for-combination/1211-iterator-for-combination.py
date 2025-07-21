bd1c560467d2d55bb93601026e63129067994fad
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-21 17:17:05 -0500

    Time: 15 ms (38.38%), Space: 21.8 MB (6.69%) - LeetHub

--- 1211-iterator-for-combination/1211-iterator-for-combination.py
+++ 1211-iterator-for-combination/1211-iterator-for-combination.py
@@ -1,23 +1,28 @@
 class CombinationIterator:
 
     def __init__(self, characters: str, combinationLength: int):
-        self.curr = 0
         self.combinations = []
-        comb = []
-        def dfs(i):
-            if len(comb) == combinationLength:
-                self.combinations.append("".join(comb))
+        self.curr = 0
+        path = []
+        def backtrack(index):
+            if index == len(characters):
+                if len(path) == combinationLength:
+                    self.combinations.append("".join(path))
                 return
-            if i == len(characters):
-                return
-            comb.append(characters[i])
-            dfs(i + 1)
-            comb.pop()
-            dfs(i + 1)
-        dfs(0)
+            path.append(characters[index])
+            backtrack(index + 1)
+            path.pop()
+            backtrack(index + 1)
+        backtrack(0)
     def next(self) -> str:
-        result = self.combinations[self.curr]
+        s = self.combinations[self.curr]
         self.curr += 1
-        return result
+        return s
     def hasNext(self) -> bool:
         return self.curr < len(self.combinations)
+
+
+# Your CombinationIterator object will be instantiated and called as such:
+# obj = CombinationIterator(characters, combinationLength)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
