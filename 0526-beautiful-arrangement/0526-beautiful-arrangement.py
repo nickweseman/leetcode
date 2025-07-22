efcb4351cab9a25df253e75e4e557150108778c6
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-22 10:06:50 -0500

    Time: 859 ms (25.35%), Space: 17.7 MB (76.84%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,19 +1,19 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        total = 0
+        num_beautiful = 0
+        path = []
         used = [False] * (n + 1)
         def backtrack(index):
-            nonlocal total
+            nonlocal num_beautiful
             if index == n + 1:
-                total += 1
+                num_beautiful += 1
                 return
             for i in range(1, n + 1):
-                if used[i]:
-                    continue
-                if index % i == 0 or i % index == 0:
+                if (index % i == 0 or i % index == 0) and not used[i]:
                     used[i] = True
+                    path.append(i)
                     backtrack(index + 1)
+                    path.pop()
                     used[i] = False
         backtrack(1)
-        return total
-            
+        return num_beautiful
