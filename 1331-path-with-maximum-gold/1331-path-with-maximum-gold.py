7a36e23c5aefa7e8789f9ea23d4ffadb58bb2b16
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-23 06:14:35 -0500

    Time: 4217 ms (21.36%), Space: 18.1 MB (13.55%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -2,17 +2,19 @@ class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         visited = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         def backtrack(r, c):
-            if not (0 <= r < ROWS and 0 <= c < COLS):
+            if not (0 <= r < ROWS and 0 <= c < COLS) or (r,c) in visited or grid[r][c] == 0:
                 return 0
-            if grid[r][c] == 0 or (r, c) in visited:
-                return 0
+            original_gold = grid[r][c]
+            grid[r][c] = 0
             visited.add((r, c))
             gold_from_neighbors = 0
-            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            for dr, dc in directions:
                 gold_from_neighbors = max(gold_from_neighbors, backtrack(r + dr, c + dc))
+            grid[r][c] = original_gold
             visited.remove((r, c))
-            return grid[r][c] + gold_from_neighbors
+            return original_gold + gold_from_neighbors
         max_gold = 0
         for r in range(ROWS):
             for c in range(COLS):
