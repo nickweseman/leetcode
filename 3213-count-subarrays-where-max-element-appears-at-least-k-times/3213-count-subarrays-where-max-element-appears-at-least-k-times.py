e9a813373088166a36017db1f52d87d7473c64bc
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-23 08:13:37 -0500

    Time: 130 ms (18.53%), Space: 29.7 MB (39.62%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,18 +1,20 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         maxx = max(nums)
-        max_count = 0
-        total_subarrays = (len(nums) * (len(nums) + 1)) // 2
-        left = right = 0
-        subarrays = 0
-
-        while right < len(nums):
-            if nums[right] == maxx:
-                max_count += 1
-            while left <= right and max_count > (k - 1):
-                if nums[left] == maxx:
-                    max_count -= 1
-                left += 1
-            subarrays += right - left + 1
-            right += 1
-        return total_subarrays - subarrays
+        n = len(nums)
+        total_subarrays = (n * (n + 1)) // 2
+        def atMostK(k_) -> int:
+            left = right = 0
+            count = 0
+            subarrays = 0
+            while right < len(nums):
+                if nums[right] == maxx:
+                    count += 1
+                while count > k_:
+                    if nums[left] == maxx:
+                        count -= 1
+                    left += 1
+                subarrays += right - left + 1
+                right += 1
+            return subarrays
+        return total_subarrays - atMostK(k - 1)
