5fc2bde469685b55f6220a3eedfb8178dec0ce6d
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-23 12:16:35 -0500

    Time: 23 ms (75.85%), Space: 18 MB (52.63%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -4,19 +4,19 @@ class Solution:
         if total % k != 0:
             return False
         target_sum = total // k
-        buckets = [0] * k
         n = len(nums)
+        sums = [0] * k
         nums.sort(reverse=True)
         def backtrack(index):
             if index == n:
                 return True
             for i in range(k):
-                if buckets[i] + nums[index] <= target_sum:
-                    buckets[i] += nums[index]
+                if sums[i] + nums[index] <= target_sum:
+                    sums[i] += nums[index]
                     if backtrack(index + 1):
                         return True
-                    buckets[i] -= nums[index]
-                    if buckets[i] == 0:
+                    sums[i] -= nums[index]
+                    if sums[i] == 0:
                         break
             return False
         return backtrack(0)
