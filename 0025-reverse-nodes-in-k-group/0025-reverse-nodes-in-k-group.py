9798d4b9eb9b566ea2a6348af955f341f34bfd80
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-23 17:01:40 -0500

    Time: 2 ms (37.04%), Space: 18.5 MB (59.51%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,14 +5,15 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        def k_nodes_exist(node) -> bool:
+            for _ in range(k):
+                node = node.next
+                if not node:
+                    return False
+            return True
         dummy = ListNode(0, head)
         node_before_reversal = dummy
-        while True:
-            kth_node = node_before_reversal
-            for _ in range(k):
-                kth_node = kth_node.next
-                if not kth_node:
-                    return dummy.next
+        while k_nodes_exist(node_before_reversal):
             reversal_tail = node_before_reversal.next
             prev = None
             curr = reversal_tail
@@ -21,3 +22,4 @@ class Solution:
             node_before_reversal.next = prev
             reversal_tail.next = curr
             node_before_reversal = reversal_tail
+        return dummy.next
