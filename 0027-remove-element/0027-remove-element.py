16b9b1765631be9cd88fe9fcdc8c373495f34e5a
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-24 15:22:09 -0500

    Time: 1 ms (5.44%), Space: 17.7 MB (82.74%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,7 +1,6 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         read = write = 0
-
         while read < len(nums):
             if nums[read] != val:
                 nums[read], nums[write] = nums[write], nums[read]
