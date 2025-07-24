b73164b4c51c50c72c0c8a306320badbb8b04f84
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-24 08:27:20 -0500

    Time: 299 ms (18.48%), Space: 31.1 MB (79.64%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,21 +1,17 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         window = collections.deque()
-        answer = []
         left = right = 0
-        
-        def add() -> None:
+        result = []
+        def add():
             while window and nums[window[-1]] < nums[right]:
                 window.pop()
             window.append(right)
-
-        def remove() -> None:
-            if window and window[0] == left:
+        def remove():
+            if window[0] == left:
                 window.popleft()
-
-        def update_answer() -> None:
-            answer.append(nums[window[0]])
-
+        def update_answer():
+            result.append(nums[window[0]])
         while right < len(nums):
             add()
             if right - left + 1 > k:
@@ -24,4 +20,4 @@ class Solution:
             if right - left + 1 == k:
                 update_answer()
             right += 1
-        return answer
+        return result
