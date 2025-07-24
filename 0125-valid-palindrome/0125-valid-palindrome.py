27d8c62e9dafc137c24093573c7300ebc4d94478
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-24 09:48:45 -0500

    Time: 10 ms (39.88%), Space: 17.8 MB (95.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         left, right = 0, len(s) - 1
-
         while left < right:
             if not s[left].isalnum():
                 left += 1
