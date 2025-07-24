6e0ab45a77c00e976e90907f2a7c40671f006257
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-24 08:32:53 -0500

    Time: 47 ms (5.17%), Space: 18.6 MB (94.55%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,17 +1,15 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def helper(g: int) -> int:
+        def numSubarrays(goal_):
+            subarrays = 0
             left = right = 0
             total = 0
-            subarrays = 0
-
             while right < len(nums):
                 total += nums[right]
-
-                while left <= right and total > g:
+                while left <= right and total > goal_:
                     total -= nums[left]
                     left += 1
                 subarrays += right - left + 1
                 right += 1
             return subarrays
-        return helper(goal) - helper(goal - 1)
+        return numSubarrays(goal) - numSubarrays(goal - 1)
