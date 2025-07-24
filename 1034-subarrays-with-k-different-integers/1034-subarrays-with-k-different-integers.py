2666db6fb3fdecb64ca492625fb0ee39a2dfff97
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-24 15:30:56 -0500

    Time: 145 ms (27.65%), Space: 19.5 MB (99.35%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.py
@@ -1,19 +1,17 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        def helper(kk: int):
-            subarrays = 0
+        def subarrays(k_):
             left = right = 0
             window = collections.defaultdict(int)
-
+            subarrays = 0
             while right < len(nums):
                 window[nums[right]] += 1
-
-                while len(window) > kk:
+                while len(window) > k_:
                     window[nums[left]] -= 1
                     if window[nums[left]] == 0:
                         del window[nums[left]]
                     left += 1
                 subarrays += right - left + 1
-                right += 1
+                right += 1             
             return subarrays
-        return helper(k) - helper(k - 1)
+        return subarrays(k) - subarrays(k - 1)
