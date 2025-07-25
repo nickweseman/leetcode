3d3511ee03e52af048a2093630cc623ace20a832
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-25 10:20:33 -0500

    Time: 0 ms (100%), Space: 17.8 MB (67.53%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,13 +1,14 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        values = [0] * 1_001
+        counts = collections.Counter(arr1)
+        buckets = [0] * 1_001
+        for i, freq in counts.items():
+            buckets[i] = freq
         result = []
-        for num in arr1:
-            values[num] += 1
         for num in arr2:
-            result.extend([num] * values[num])
-            values[num] = 0
-        for i, num in enumerate(values):
-            if num:
-                result.extend([i] * num)
+            result.extend([num] * buckets[num])
+            buckets[num] = 0
+        for i in range(1_001):
+            if buckets[i] > 0:
+                result.extend([i] * buckets[i])
         return result
