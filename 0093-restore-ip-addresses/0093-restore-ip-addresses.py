b6a1d2261b6502bc357ee8270d58491c3c67a142
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-25 06:50:40 -0500

    Time: 23 ms (11.75%), Space: 18 MB (28.66%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,23 +1,19 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        n = len(s)
         path = []
         result = []
-        n = len(s)
         def backtrack(index):
             if index == n:
                 if len(path) == 4:
                     result.append(".".join(path))
                 return
-            if len(path) == 4:
-                return
             for i in range(index, n):
-                substring = s[index: i + 1]
-                if len(substring) > 1 and substring[0] == "0":
-                    continue
-                if not (0 <= int(substring) <= 255):
-                    continue
-                path.append(substring)
-                backtrack(i + 1)
-                path.pop()
+                segment = s[index : i + 1]
+                
+                if (0 <= int(segment) <= 255) and (len(segment) == 1 or (len(segment) > 1 and segment[0] != "0")):
+                    path.append(segment)
+                    backtrack(i + 1)
+                    path.pop()
         backtrack(0)
         return result
