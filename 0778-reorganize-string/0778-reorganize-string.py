25600cf80ad86e73bd47f69677e45c9d70c5c419
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-25 06:38:57 -0500

    Time: 3 ms (47.2%), Space: 17.8 MB (74.81%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,20 +1,28 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        max_heap = []
         counts = collections.Counter(s)
-        heap = []
+        n = len(s)
         for c, freq in counts.items():
-            if freq > math.ceil(len(s) / 2):
+            if freq > (n + 1) // 2:
                 return ""
-            heap.append((-freq, c))
-        heapq.heapify(heap)
+            max_heap.append((-freq, c))
+        heapq.heapify(max_heap)
         result = []
-        prev_freq, prev_c = 0, ""
-        for _ in range(len(s)):
-            neg_freq, c = heapq.heappop(heap)
-            freq = -neg_freq
-            result.append(c)
-            if prev_c:
-                heapq.heappush(heap, (-prev_freq, prev_c))
-            prev_c = c
-            prev_freq = freq - 1
+        while max_heap:
+            neg_freq1, c1 = heapq.heappop(max_heap)
+            freq1 = -neg_freq1
+            if result and result[-1] == c1:
+                if not max_heap:
+                    return ""
+                neg_freq2, c2 = heapq.heappop(max_heap)
+                freq2 = -neg_freq2
+                result.append(c2)
+                freq2 -= 1
+                if freq2 > 0:
+                    heapq.heappush(max_heap, (-freq2, c2))
+            result.append(c1)
+            freq1 -= 1
+            if freq1 > 0:
+                heapq.heappush(max_heap, (-freq1, c1))
         return "".join(result)
