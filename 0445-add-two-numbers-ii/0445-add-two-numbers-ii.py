4d965657de4002934a012a04ac937c3d5086a6cb
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-26 11:27:19 -0500

    Time: 11 ms (14.16%), Space: 17.9 MB (57.69%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,26 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse_list(head):
-            prev = None
-            curr = head
-            while curr:
-                curr.next, curr, prev = prev, curr.next, curr
-            return prev
-        l1 = reverse_list(l1)
-        l2 = reverse_list(l2)
-        dummy = ListNode()
-        curr = dummy
+        stack1 = []
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        stack2 = []
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
         carry = 0
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        result_head = None
+        while stack1 or stack2 or carry:
+            val1 = stack1.pop() if stack1 else 0
+            val2 = stack2.pop() if stack2 else 0
             total = val1 + val2 + carry
             carry = total // 10
-            curr.next = ListNode(total % 10)
-            curr = curr.next
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-        return reverse_list(dummy.next)
+            curr = ListNode(total % 10)
+            curr.next = result_head
+            result_head = curr
+        return result_head
