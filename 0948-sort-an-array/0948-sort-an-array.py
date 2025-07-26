3371f17fdde3bb2c2a48e8250232c301c41116a8
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-26 10:38:21 -0500

    Time: 167 ms (79.37%), Space: 27 MB (17.99%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -4,9 +4,8 @@ class Solution:
         offset = -min_val
         size = max_val - min_val + 1
         buckets = [0] * size
-        counts = collections.Counter(nums)
-        for num, freq in counts.items():
-            buckets[num + offset] = freq
+        for num in nums:
+            buckets[num + offset] += 1
         result = []
         for num in range(size):
             result.extend([num - offset] * buckets[num])
