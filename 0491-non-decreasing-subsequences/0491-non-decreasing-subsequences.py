8e0c9c7386cec19e5b6f398c81962c6581076d99
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-27 11:23:34 -0500

    Time: 23 ms (56.82%), Space: 24.7 MB (42.25%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -5,11 +5,11 @@ class Solution:
         n = len(nums)
         def backtrack(index):
             if index == n:
+                if len(path) >= 2:
+                    result_set.add(tuple(path))
                 return
             if not path or nums[index] >= path[-1]:
                 path.append(nums[index])
-                if len(path) >= 2:
-                    result_set.add(tuple(path))
                 backtrack(index + 1)
                 path.pop()
             backtrack(index + 1)
