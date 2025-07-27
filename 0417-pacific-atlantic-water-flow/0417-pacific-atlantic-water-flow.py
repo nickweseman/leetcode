29d28aa16b34bd39190e7be32115f377ea0e2035
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-27 09:20:32 -0500

    Time: 23 ms (93.73%), Space: 19.3 MB (80.68%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,23 +1,18 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
         pacific, atlantic = set(), set()
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        rows, cols = len(heights), len(heights[0])
+        neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         def dfs(r, c, visited, prev_height):
             if not (0 <= r < rows and 0 <= c < cols) or (r, c) in visited or heights[r][c] < prev_height:
                 return
             visited.add((r, c))
-            for dr, dc in directions:
+            for dr, dc in neighbors:
                 dfs(r + dr, c + dc, visited, heights[r][c])
+        for r in range(rows):
+            dfs(r, 0, pacific, 0)
+            dfs(r, cols - 1, atlantic, 0)
         for c in range(cols):
-            dfs(0, c, pacific, heights[0][c])
-            dfs(rows - 1, c, atlantic, heights[rows - 1][c])
-        for r in range(rows):
-            dfs(r, 0, pacific, heights[r][0])
-            dfs(r, cols - 1, atlantic, heights[r][c])
-        result = []
-        for r in range(rows):
-            for c in range(cols):
-                if (r, c) in pacific and (r, c) in atlantic:
-                    result.append([r, c])
-        return result
+            dfs(0, c, pacific, 0)
+            dfs(rows - 1, c, atlantic, 0)
+        return list(pacific & atlantic)
