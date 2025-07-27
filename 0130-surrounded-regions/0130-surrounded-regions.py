8673e5bff4da22ca818bb9deee268e6dc52a6b72
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-27 09:54:57 -0500

    Time: 7 ms (55.14%), Space: 21.7 MB (72.03%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,28 +1,24 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         rows, cols = len(board), len(board[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         def dfs(r, c):
             if not (0 <= r < rows and 0 <= c < cols) or board[r][c] != "O":
                 return
             board[r][c] = "T"
-            for dr, dc in directions:
+            for dr, dc in neighbors:
                 dfs(r + dr, c + dc)
-        # 1. (DFS) Capture unsurrounded regions (O -> T)
         for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "O" and r in (0, rows - 1) or c in (0, cols - 1):
-                    dfs(r, c)
-        # 2. Capture surrounded regions (O -> X)
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-        # 3.  Uncapture unsurrounded regions (T -> O)
+            dfs(r, 0)
+            dfs(r, cols - 1)
+        for c in range(cols):
+            dfs(0, c)
+            dfs(rows - 1, c)
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == "T":
                     board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
         
-
         
