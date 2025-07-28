e4fd855dd6389ffe70d877bb3283c60f6bdc6966
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-28 09:12:26 -0500

    Time: 3 ms (46.05%), Space: 18.8 MB (92.74%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -9,15 +9,14 @@ class Solution:
         path = []
         result = []
         def dfs(node, sum_so_far):
-            if not node:
-                return None
-            my_sum = sum_so_far + node.val
-            path.append(node.val)
-            if not node.left and not node.right:
-                if targetSum == my_sum:
+            if node:
+                path.append(node.val)
+                total = sum_so_far + node.val
+                if not node.left and not node.right and total == targetSum:
                     result.append(path.copy())
-            dfs(node.left, my_sum)
-            dfs(node.right, my_sum)
-            path.pop()
+                else:
+                    dfs(node.left, total)
+                    dfs(node.right, total)
+                path.pop()
         dfs(root, 0)
         return result
