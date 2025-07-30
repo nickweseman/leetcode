5f67402cbc79c7575178a1517c22f327cd8d3ecc
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-30 17:06:04 -0500

    Time: 33 ms (82.53%), Space: 32.5 MB (75.09%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -1,23 +1,16 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         parent = list(range(n))
-        rank = [1] * n
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
             return parent[x]
         def union(x, y):
-            nonlocal networks
             px, py = find(x), find(y)
             if px == py:
                 return False
             else:
-                if rank[px] < rank[py]:
-                    parent[px] = py
-                    rank[py] += rank[px]
-                else:
-                    parent[py] = px
-                    rank[px] += rank[py]
+                parent[px] = py
                 return True
         networks = n
         for a, b in connections:
