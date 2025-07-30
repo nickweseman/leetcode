49e3c620bd6010df99a4776b81f529f2b75b493c
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-30 07:27:53 -0500

    Time: 0 ms (100%), Space: 18.1 MB (93.94%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,27 +1,24 @@
-class UnionFind:
-    def __init__(self, edges):
-        n = len(edges) + 1
-        self.parent = list(range(n))
-        self.rank = [1] * n
-    def find(self, node):
-        if self.parent[node] != node:
-            self.parent[node] = self.find(self.parent[node])
-        return self.parent[node]
-    def union(self, node1, node2):
-        p1, p2 = self.find(node1), self.find(node2)
-        if p1 != p2:
-            if self.rank[p1] < self.rank[p2]:
-                self.parent[p1] = p2
-                self.rank[p2] += self.rank[p1]
-            else:
-                self.parent[p2] = p1
-                self.rank[p1] += self.rank[p2]
-            return True
-        else:
-            return False
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        uf = UnionFind(edges)
+        n = len(edges) + 1
+        parent = list(range(n))
+        rank = [1] * n
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        def union(u, v):
+            pu, pv = find(u), find(v)
+            if pu != pv:
+                if rank[pu] < rank[pv]:
+                    parent[pu] = pv
+                    rank[pu] += rank[pv]
+                else:
+                    parent[pu] = pv
+                    rank[pu] += rank[pv]
+                return True
+            else:
+                return False
         for edge in edges:
-            if not uf.union(edge[0], edge[1]):
+            if not union(edge[0], edge[1]):
                 return edge
