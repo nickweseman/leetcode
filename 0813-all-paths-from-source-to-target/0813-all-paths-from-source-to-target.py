763cf19d7bcb0084458b6dae03b3ecb25b44e730
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-31 14:38:01 -0500

    Time: 3 ms (93.01%), Space: 19.4 MB (32.19%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,14 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        path = [0]
         result = []
+        path = [0]
+        n = len(graph)
         def backtrack(node):
-            if node == len(graph) - 1:
+            if node == n - 1:
                 result.append(path.copy())
                 return
-            for i in graph[node]:
-                path.append(i)
-                backtrack(i)
+            for nei in graph[node]:
+                path.append(nei)
+                backtrack(nei)
                 path.pop()
         backtrack(0)
         return result
