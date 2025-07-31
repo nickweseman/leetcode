7508421aee19233d97109cbae34d608ae03bfc2b
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-31 12:33:02 -0500

    Time: 3158 ms (51.17%), Space: 17.7 MB (94.75%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -1,20 +1,19 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        def backtrack(r, c):
-            if not (0 <= r < ROWS and 0 <= c < COLS) or grid[r][c] == 0:
+        rows, cols = len(grid), len(grid[0])
+        def dfs(r, c):
+            if not (0 <= r < rows and 0 <= c < cols) or grid[r][c] == 0:
                 return 0
-            original_gold = grid[r][c]
+            gold = grid[r][c]
             grid[r][c] = 0
-            gold_from_neighbors = 0
+            max_gold_from_neighbors = 0
             for dr, dc in directions:
-                gold_from_neighbors = max(gold_from_neighbors, backtrack(r + dr, c + dc))
-            grid[r][c] = original_gold
-            return original_gold + gold_from_neighbors
+                max_gold_from_neighbors = max(max_gold_from_neighbors, dfs(r + dr, c + dc))
+            grid[r][c] = gold
+            return gold + max_gold_from_neighbors
         max_gold = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] != 0:
-                    max_gold = max(max_gold, backtrack(r, c))
+        for r in range(rows):
+            for c in range(cols):
+                max_gold = max(max_gold, dfs(r, c))
         return max_gold
