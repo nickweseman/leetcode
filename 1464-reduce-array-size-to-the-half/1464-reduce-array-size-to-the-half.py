a50d26c6c95a92fc1182a922bc9b22e5537896e8
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-31 17:30:52 -0500

    Time: 60 ms (45.29%), Space: 41.1 MB (34.19%) - LeetHub

--- 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.py
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.py
@@ -1,18 +1,12 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        counter = collections.Counter(arr)
-        max_freq = len(arr) + 1
-        buckets = [0 for _ in range(max_freq)]
-        for num, freq in counter.items():
-            buckets[freq] += 1
-        size = len(arr) // 2
         count = 0
-        for i in reversed(range(max_freq)):
-            for j in range(buckets[i]):
-                size -= i
-                count += 1
-                if size <= 0:
-                    return count
+        half = len(arr) / 2
+        counts = collections.Counter(arr)
+        size = 0
+        for i, freq in counts.most_common():
+            size += freq
+            count += 1
+            if size >= half:
+                return count
         return count
-
-
