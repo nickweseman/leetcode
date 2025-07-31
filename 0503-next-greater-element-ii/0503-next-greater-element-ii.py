d4a8f2164174b359638432dbec029b55f050413f
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-31 15:22:41 -0500

    Time: 19 ms (82.08%), Space: 19.3 MB (75.24%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -3,13 +3,10 @@ class Solution:
         n = len(nums)
         result = [-1] * n
         stack = []
-
         for i in range(2 * n):
             actual_i = i % n
-            num = nums[actual_i]
-
-            while stack and nums[stack[-1]] < num:
-                result[stack.pop()] = num
+            while stack and nums[stack[-1]] < nums[actual_i]:
+                result[stack.pop()] = nums[actual_i]
             if i < n:
                 stack.append(i)
         return result
