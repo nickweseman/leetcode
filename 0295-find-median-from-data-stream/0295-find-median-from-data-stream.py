f53f62a8917bb8cb456b88c823bb8d0974fdfb60
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-31 16:14:33 -0500

    Time: 128 ms (75.86%), Space: 39.1 MB (93.68%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -3,7 +3,6 @@ class MedianFinder:
     def __init__(self):
         self.min_heap = []
         self.max_heap = []
-
     def addNum(self, num: int) -> None:
         if len(self.min_heap) == len(self.max_heap):
             val = -heapq.heappushpop(self.max_heap, -num)
@@ -11,10 +10,9 @@ class MedianFinder:
         else:
             val = heapq.heappushpop(self.min_heap, num)
             heapq.heappush(self.max_heap, -val)
-
     def findMedian(self) -> float:
         if len(self.min_heap) == len(self.max_heap):
-            return (self.min_heap[0] + -self.max_heap[0]) / 2 
+            return (self.min_heap[0] + -self.max_heap[0]) / 2
         else:
             return self.min_heap[0]
         
