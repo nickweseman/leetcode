56962748260df8148f304b59c3874ead4df60a6d
Nick Weseman <trumpernickel2@yahoo.com>
2025-07-31 09:09:54 -0500

    Time: 7 ms (26.88%), Space: 19.9 MB (6.36%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,24 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        pre_map = collections.defaultdict(list)
+        graph = collections.defaultdict(list)
         for course, pre in prerequisites:
-            pre_map[course].append(pre)
-        cycle, visited = set(), set()
+            graph[course].append(pre)
+        cycle = set()
+        visited = set()
         output = []
         def dfs(course):
             if course in cycle:
                 return False
-            if course in visited:
-                return True
             cycle.add(course)
-            for pre in pre_map[course]:
-                if not dfs(pre):
-                    return False
+            for pre in graph[course]:
+                if pre not in visited:
+                    if not dfs(pre):
+                        return False
             cycle.remove(course)
-            output.append(course)
             visited.add(course)
+            output.append(course)
             return True
         for course in range(numCourses):
-            if not dfs(course):
-                return []
+            if course not in visited:
+                if not dfs(course):
+                    return []
         return output
