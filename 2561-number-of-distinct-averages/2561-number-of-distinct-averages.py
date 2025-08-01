5fcf6d0a7c2dd9d404558ba2f693a81ee25cde14
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-01 13:07:32 -0500

    Time: 0 ms (100%), Space: 17.8 MB (42.62%) - LeetHub

--- 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.py
@@ -3,7 +3,6 @@ class Solution:
         avgs = set()
         left, right = 0, len(nums) - 1
         nums.sort()
-        
         while left < right:
             avgs.add((nums[left] + nums[right]) / 2)
             left += 1
