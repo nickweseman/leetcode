345143866887446c8bbbbf6aa90198ccd184225f
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-01 08:29:14 -0500

    Time: 31 ms (70.83%), Space: 19.5 MB (46.91%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,13 +1,14 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific, atlantic = set(), set()
         rows, cols = len(heights), len(heights[0])
-        neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        pacific, atlantic = set(), set()
+        result = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         def dfs(r, c, visited, prev_height):
-            if not (0 <= r < rows and 0 <= c < cols) or (r, c) in visited or heights[r][c] < prev_height:
+            if not (0 <= r < rows and 0 <= c < cols) or heights[r][c] < prev_height or (r, c) in visited:
                 return
             visited.add((r, c))
-            for dr, dc in neighbors:
+            for dr, dc in directions:
                 dfs(r + dr, c + dc, visited, heights[r][c])
         for r in range(rows):
             dfs(r, 0, pacific, 0)
