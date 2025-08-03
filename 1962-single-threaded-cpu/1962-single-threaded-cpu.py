b1f3be33fd6c1039f94801106cb93e4fbea167f9
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-03 08:29:31 -0500

    Time: 413 ms (31.97%), Space: 56.9 MB (89.61%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -2,22 +2,20 @@ class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         tasks_min_heap = []
         for i, task in enumerate(tasks):
-            start_time, processing_time = task
-            heapq.heappush(tasks_min_heap, (start_time, i, processing_time))
+            start_time, p_time = task
+            tasks_min_heap.append((start_time, i, p_time))
         heapq.heapify(tasks_min_heap)
-        processing_min_heap = []
-        time = 1
+        waiting_min_heap = []
         result = []
-        while tasks_min_heap or processing_min_heap:
+        time = 1
+        while tasks_min_heap or waiting_min_heap:
             while tasks_min_heap and tasks_min_heap[0][0] <= time:
-                _, i, processing_time = heapq.heappop(tasks_min_heap)
-                heapq.heappush(processing_min_heap, (processing_time, i))
-            if processing_min_heap:
-                processing_time, i = heapq.heappop(processing_min_heap)
-                time += processing_time
+                _, i, p_time = heapq.heappop(tasks_min_heap)
+                heapq.heappush(waiting_min_heap, (p_time, i))
+            if waiting_min_heap:
+                p_time, i = heapq.heappop(waiting_min_heap)
                 result.append(i)
+                time += p_time
             else:
                 time = tasks_min_heap[0][0]
         return result
-            
-
