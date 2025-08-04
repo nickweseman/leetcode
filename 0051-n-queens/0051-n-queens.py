f1d6ba5d84e6c945e7a6964386f4b7a6623742dd
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-04 08:27:58 -0500

    Time: 11 ms (72.4%), Space: 18.3 MB (34.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -3,23 +3,25 @@ class Solution:
         cols = set()
         pos_diags = set() # r - c
         neg_diags = set() # r + c
-        grid = [["."] * n for _ in range(n)]
+        board = [["."] * n for _ in range(n)]
         result = []
         def backtrack(r):
             if r == n:
-                grid_entry = ["".join(row) for row in grid]
-                result.append(grid_entry)
+                copy = []
+                for row in range(n):
+                    copy.append("".join(board[row]))
+                result.append(copy.copy())
                 return
             for c in range(n):
                 if c not in cols and r - c not in pos_diags and r + c not in neg_diags:
+                    board[r][c] = "Q"
                     cols.add(c)
                     pos_diags.add(r - c)
                     neg_diags.add(r + c)
-                    grid[r][c] = "Q"
                     backtrack(r + 1)
+                    board[r][c] = "."
                     cols.remove(c)
                     pos_diags.remove(r - c)
                     neg_diags.remove(r + c)
-                    grid[r][c] = "."
         backtrack(0)
         return result
