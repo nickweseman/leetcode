ab2f8edd6f31f2b813ec1f5ac402568adcfc8a3c
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-04 08:45:45 -0500

    Time: 15 ms (15.76%), Space: 19.1 MB (45.72%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,19 +1,22 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = provinces = len(isConnected)
+        n = len(isConnected)
         parent = list(range(n))
+        provinces = n
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
             return parent[x]
         def union(x, y):
-            nonlocal provinces
             px, py = find(x), find(y)
             if px != py:
                 parent[px] = py
-                provinces -= 1
-        for r in range(n):
-            for c in range(n):
-                if r != c and isConnected[r][c] == 1:
-                    union(r, c)
+                return True
+            else:
+                return False
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1 and i != j:
+                    if union(i, j):
+                        provinces -= 1
         return provinces
