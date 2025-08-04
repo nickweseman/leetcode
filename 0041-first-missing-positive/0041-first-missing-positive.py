283182ccdf968b2154d87c80aa4b1719bdb3a7a5
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-04 11:20:46 -0500

    Time: 63 ms (19.94%), Space: 29 MB (45.1%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -10,7 +10,7 @@ class Solution:
                 if nums[val - 1] > 0:
                     nums[val - 1] *= -1
                 elif nums[val - 1] == 0:
-                    nums[val - 1] = -1 * (n + 1)
+                    nums[val - 1] = -math.inf # -1 * (n + 1)
         for i in range(1, n + 1):
             if nums[i - 1] >= 0:
                 return i
