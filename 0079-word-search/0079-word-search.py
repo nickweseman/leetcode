0d105350ead2bfac0a78ad1987426013ce69cf78
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 12:20:40 -0500

    Time: 4998 ms (26.82%), Space: 17.9 MB (64.73%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,22 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows = len(board)
-        cols = len(board[0])
-        path = []
+        rows, cols = len(board), len(board[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
+        n = len(word)
         def backtrack(r, c, index):
-            if len(path) == len(word):
+            if index == n:
                 return True
-            if not (0 <= r < rows and 0 <= c < cols) or board[r][c] != word[index]:
+            if not (0 <= r < rows and 0 <= c < cols) or (r, c) in visited or board[r][c] != word[index]:
                 return False
-            path.append(board[r][c])
-            prev = board[r][c]
-            board[r][c] = "#"
+            visited.add((r, c))
             for dr, dc in directions:
                 if backtrack(r + dr, c + dc, index + 1):
                     return True
-            path.pop()
-            board[r][c] = prev
+            visited.remove((r, c))
             return False
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == word[0]:
-                    if backtrack(r, c, 0):
-                        return True
+                if backtrack(r, c, 0):
+                    return True
         return False
