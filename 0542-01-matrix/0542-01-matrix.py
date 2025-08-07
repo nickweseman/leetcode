efc0b6b8ede42aaa0382779d769b739fb04e8883
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 15:20:58 -0500

    Time: 103 ms (92.15%), Space: 20.5 MB (56.39%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,23 +4,18 @@ class Solution:
         result = [[-1] * cols for _ in range(rows)]
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         queue = collections.deque()
-        visited = set()
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 0:
                     result[r][c] = 0
-                    visited.add((r, c))
                     queue.append((r, c))
-        time = 1
         while queue:
             for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if not (0 <= nr < rows and 0 <= nc < cols) or (nr, nc) in visited or result[nr][nc] != -1:
+                    if not (0 <= nr < rows and 0 <= nc < cols) or result[nr][nc] != -1:
                         continue
-                    visited.add((nr, nc))
-                    result[nr][nc] = time
+                    result[nr][nc] = result[r][c] + 1
                     queue.append((nr, nc))
-            time += 1
         return result
