8f93df7228f97bb091ebd34a6ae12d264f5bbebb
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 09:26:13 -0500

    Time: 1223 ms (52.97%), Space: 146.4 MB (32%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,23 +1,18 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        adj = collections.defaultdict(list)
         n = len(points)
-        for i in range(n):
-            x1, y1 = points[i]
-            for j in range(i + 1, n):
-                x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj[i].append((dist, j))
-                adj[j].append((dist, i))
-        visited = set()
         min_heap = [(0, 0)]
+        visited = set()
         total_cost = 0
         while len(visited) < n:
-            cost, node = heapq.heappop(min_heap)
-            if node in visited:
+            dist, i = heapq.heappop(min_heap)
+            if i in visited:
                 continue
-            total_cost += cost
-            visited.add(node)
-            for nei_cost, nei in adj[node]:
-                heapq.heappush(min_heap, ((nei_cost, nei)))
+            total_cost += dist
+            x1, y1 = points[i]
+            visited.add(i)
+            for j in range(n):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                heapq.heappush(min_heap, (dist, j))
         return total_cost
