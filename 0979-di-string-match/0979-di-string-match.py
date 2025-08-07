d7a4b6785187a602b43b7d5aa2bc1e97adb6c7cf
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 07:32:29 -0500

    Time: 0 ms (100%), Space: 18.7 MB (81.17%) - LeetHub

--- 0979-di-string-match/0979-di-string-match.py
+++ 0979-di-string-match/0979-di-string-match.py
@@ -1,14 +1,13 @@
 class Solution:
     def diStringMatch(self, s: str) -> List[int]:
-        perm = []
-        low, high = 0, len(s)
-
+        left, right = 0, len(s)
+        result = []
         for c in s:
             if c == "I":
-                perm.append(low)
-                low += 1
+                result.append(left)
+                left += 1
             else:
-                perm.append(high)
-                high -= 1
-        perm.append(low)
-        return perm
+                result.append(right)
+                right -= 1
+        result.append(left)
+        return result
