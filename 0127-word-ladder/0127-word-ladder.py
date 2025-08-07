7eb23f7540659280ccf90cb974d3417e4b535f28
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 11:19:15 -0500

    Time: 55 ms (85.64%), Space: 20.9 MB (40.36%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -3,13 +3,13 @@ class Solution:
         if endWord not in wordList:
             return 0
         wordList.append(beginWord)
-        pattern_to_words = collections.defaultdict(list)
+        patterns = collections.defaultdict(list)
         for word in wordList:
             for i in range(len(word)):
                 pattern = word[:i] + "*" + word[i+1:]
-                pattern_to_words[pattern].append(word)
-        visited = set([beginWord])
+                patterns[pattern].append(word)
         queue = collections.deque([beginWord])
+        visited = set()
         time = 1
         while queue:
             for _ in range(len(queue)):
@@ -18,12 +18,9 @@ class Solution:
                     return time
                 for i in range(len(word)):
                     pattern = word[:i] + "*" + word[i+1:]
-                    for nei in pattern_to_words[pattern]:
+                    for nei in patterns[pattern]:
                         if nei not in visited:
-                            queue.append(nei)
                             visited.add(nei)
+                            queue.append(nei)
             time += 1
         return 0
-        
-        
-                
