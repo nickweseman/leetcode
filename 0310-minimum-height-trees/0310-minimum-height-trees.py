55a781cb85cbe33d451c7e56cb0230c2ac7af738
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 10:51:16 -0500

    Time: 74 ms (20.82%), Space: 26.4 MB (62.97%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -2,22 +2,22 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
+        leaves_queue = collections.deque()
+        edge_count = collections.Counter()
         adj = collections.defaultdict(list)
         for a, b in edges:
             adj[a].append(b)
             adj[b].append(a)
-        leaves = collections.deque()
-        edge_count = {}
         for node, neighbors in adj.items():
             edge_count[node] = len(neighbors)
             if edge_count[node] == 1:
-                leaves.append(node)
-        while n > 2:
-            for _ in range(len(leaves)):
-                node = leaves.popleft()
+                leaves_queue.append(node)
+        while leaves_queue and n > 2:
+            for _ in range(len(leaves_queue)):
+                leaf_node = leaves_queue.popleft()
                 n -= 1
-                for nei in adj[node]:
+                for nei in adj[leaf_node]:
                     edge_count[nei] -= 1
                     if edge_count[nei] == 1:
-                        leaves.append(nei)
-        return list(leaves)
+                        leaves_queue.append(nei)
+        return list(leaves_queue)
