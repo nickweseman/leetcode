5bca5ab97cf0d974d1b9cd41e76cb66bb8c26f84
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 07:26:09 -0500

    Time: 28 ms (84.78%), Space: 20.5 MB (44.72%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.py
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -1,19 +1,17 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         pre_map = collections.defaultdict(set)
-        graph = collections.defaultdict(list)
-        for pre in prerequisites:
-            graph[pre[1]].append(pre[0])
-        def dfs(course, neighbor, visited):
-            if neighbor in visited:
-                return
-            visited.add(neighbor)
-            pre_map[course].add(neighbor)
-            for nei in graph[neighbor]:
-                dfs(course, nei, visited)
+        adj = collections.defaultdict(list)
+        for pre, course in prerequisites:
+            adj[course].append(pre)
+        def dfs(course):
+            if course in pre_map:
+                return pre_map[course]
+            for nei in adj[course]:
+                pre_map[course] |= dfs(nei)
+            pre_map[course] |= {course}
+            return pre_map[course]
         for course in range(numCourses):
-            dfs(course, course, set())
-        result = []
-        for query in queries:
-            result.append(query[0] in pre_map[query[1]])
-        return result
+            dfs(course)
+        return [q[0] in pre_map[q[1]] for q in queries]
+        
