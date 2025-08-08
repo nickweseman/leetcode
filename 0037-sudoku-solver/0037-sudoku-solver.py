da31d4229316a16fa70c3341314ca9bbe6557bc7
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-08 13:56:55 -0500

    Time: 1736 ms (55.13%), Space: 18 MB (62.31%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,36 +1,32 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
+        num_rows, num_cols = len(board), len(board[0])
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
         boxes = collections.defaultdict(set)
-        num_rows, num_cols = len(board), len(board[0])
-        empty_positions = []
+        empty_queue = []
         for r in range(num_rows):
             for c in range(num_cols):
                 if board[r][c] == ".":
-                    empty_positions.append((r, c))
+                    empty_queue.append((r, c))
                 else:
-                    num = board[r][c]
-                    rows[r].add(num)
-                    cols[c].add(num)
-                    boxes[(r//3,c//3)].add(num)
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    boxes[(r//3, c//3)].add(board[r][c])
         def backtrack(index):
-            if index == len(empty_positions):
+            if index == len(empty_queue):
                 return True
-            r, c = empty_positions[index]
-            for i in range(1, 10):
-                digit = str(i)
-                if digit not in rows[r] and digit not in cols[c] and digit not in boxes[(r//3,c//3)]:
-                    rows[r].add(digit)
-                    cols[c].add(digit)
-                    boxes[(r//3,c//3)].add(digit)
-                    board[r][c] = str(digit)
+            r, c = empty_queue[index]
+            for i in "123456789":
+                if i not in rows[r] and i not in cols[c] and i not in boxes[(r//3, c//3)]:
+                    board[r][c] = i
+                    rows[r].add(i)
+                    cols[c].add(i)
+                    boxes[(r//3, c//3)].add(i)
                     if backtrack(index + 1):
                         return True
-                    rows[r].remove(digit)
-                    cols[c].remove(digit)
-                    boxes[(r//3,c//3)].remove(digit)
                     board[r][c] = "."
-            return False
+                    rows[r].remove(i)
+                    cols[c].remove(i)
+                    boxes[(r//3, c//3)].remove(i)
         backtrack(0)
-        
