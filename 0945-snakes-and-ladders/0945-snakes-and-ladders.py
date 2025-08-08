ae6ddec89b2e994cf57e4f248021d98f1df51bbf
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-08 18:38:32 -0500

    Time: 19 ms (77.19%), Space: 18.1 MB (16.13%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,31 +1,28 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
+        board.reverse() # row 0 starts on bottom now
         n = len(board)
         square_to_pos = {}
         square = 1
-        board.reverse() # make row 0 the bottom
         for r in range(n):
             cols = range(n) if r % 2 == 0 else reversed(range(n))
             for c in cols:
                 square_to_pos[square] = (r, c)
                 square += 1
-        queue = collections.deque([1]) # square
-        visited = {1}
-        moves = 0
+        queue = collections.deque([(1, 0)]) # square, turns
+        visited = {(1, 0)}
         while queue:
-            for _ in range(len(queue)):
-                square = queue.popleft()
-                if square == n * n:
-                    return moves
-                for i in range(1, 7):
-                    next_square = square + i
-                    if next_square > n * n:
-                        break # don't overshoot the board
-                    r, c = square_to_pos[next_square]
-                    if board[r][c] != -1:
-                        next_square = board[r][c]
-                    if next_square not in visited:
-                        visited.add(next_square)
-                        queue.append((next_square))
-            moves += 1
+            square, turns = queue.popleft()
+            if square == n * n:
+                return turns
+            for i in range(1, 7):
+                next_square = square + i
+                if next_square > n * n:
+                    break
+                r, c = square_to_pos[next_square]
+                if board[r][c] != -1:
+                    next_square = board[r][c]
+                if next_square not in visited:
+                    visited.add(next_square)
+                    queue.append((next_square, turns + 1))
         return -1
