38ae79df0c1578dbffff1da0d424971ec791828a
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-08 14:55:25 -0500

    Time: 711 ms (28.9%), Space: 18 MB (76.36%) - LeetHub

--- 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
+++ 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
@@ -1,45 +1,46 @@
 class UnionFind:
     def __init__(self, n):
-        self.par = list(range(n))
+        self.parent = list(range(n))
         self.components = n
     def find(self, x):
-        if self.par[x] != x:
-            self.par[x] = self.find(self.par[x])
-        return self.par[x]
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
     def union(self, x, y):
         px, py = self.find(x), self.find(y)
         if px == py:
             return False
-        self.par[px] = py
+        self.parent[px] = py
         self.components -= 1
         return True
 class Solution:
     def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        for i, edge in enumerate(edges):
+            edge.append(i)
+        edges.sort(key = lambda x : x[2])
         critical, pseudo = [], []
-        for i in range(len(edges)):
-            edges[i].append(i) # v1, v2, w, i
-        edges.sort(key = lambda x: x[2])
         uf = UnionFind(n)
         mst_weight = 0
-        for v1, v2, w, _ in edges:
-            if uf.union(v1, v2):
+        for a, b, w, _ in edges:
+            if uf.union(a, b):
                 mst_weight += w
-        for v1, v2, w, i in edges:
+        for a1, b1, w1, i in edges:
+            # Make a mst without the edge
             uf = UnionFind(n)
             weight = 0
-            for v3, v4, w2, j in edges:
-                if i != j and uf.union(v3, v4):
+            for a2, b2, w2, j in edges:
+                if i != j and uf.union(a2, b2):
                     weight += w2
             if uf.components != 1 or weight > mst_weight:
                 critical.append(i)
                 continue
+            # Make a mst with the forced edge
             uf = UnionFind(n)
-            weight = w
-            uf.union(v1, v2)
-            for v3, v4, w2, j in edges:
-                if i != j and uf.union(v3, v4):
+            uf.union(a1, b1)
+            weight = w1
+            for a2, b2, w2, j in edges:
+                if i != j and uf.union(a2, b2):
                     weight += w2
             if uf.components == 1 and weight == mst_weight:
                 pseudo.append(i)
         return [critical, pseudo]
-        
