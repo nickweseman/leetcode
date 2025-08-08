68eb623024c8edfa3c19825787bcc2ed763483bb
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-07 22:01:37 -0500

    Time: 15 ms (89.63%), Space: 17.8 MB (82.97%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -1,20 +1,27 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        board.reverse()
+        square_to_pos = {}
+        square = 1
+        for r in range(n):
+            cols = range(n) if r % 2 == 0 else reversed(range(n))
+            for c in cols:
+                square_to_pos[square] = (r, c)
+                square += 1
+        board.reverse() # make row 0 the bottom
         def int_to_pos(square):
             r = (square - 1) // n
             c = (square - 1) % n
-            if r % 2 != 0:
-                c = n - 1 - c
+            if r % 2 == 1:
+                c = n - 1 - c # flip the column index for odd rows
             return (r, c)
         queue = collections.deque([(1, 0)]) # square, moves
-        visited = {(1, 0)}
+        visited = {1}
         while queue:
             square, moves = queue.popleft()
             for i in range(1, 7):
                 next_square = square + i
-                r, c = int_to_pos(next_square)
+                r, c = square_to_pos[next_square]
                 if board[r][c] != -1:
                     next_square = board[r][c]
                 if next_square == n * n:
