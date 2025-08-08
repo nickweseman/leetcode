e831f3ec12e9922fa5e34db5369633c0464ae349
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-08 16:02:38 -0500

    Time: 406 ms (74.58%), Space: 18.2 MB (38.83%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.py
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -1,32 +1,32 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
-        result = []
-        path = []
         n = len(num)
-        def backtrack(index, cur_eval, last_operand):
+        path = []
+        result = []
+        def backtrack(index, cur_val, last_operand):
             if index == n:
-                if cur_eval == target:
+                if cur_val == target:
                     result.append("".join(path))
                 return
             for i in range(index, n):
                 operand_str = num[index : i + 1]
-                operand_int = int(operand_str)
-                if len(operand_str) > 1 and operand_str.startswith("0"):
+                operand = int(operand_str)
+                if len(operand_str) > 1 and operand_str[0] == "0":
                     break
                 if index == 0:
                     path.append(operand_str)
-                    backtrack(i + 1, operand_int, operand_int)
+                    backtrack(i + 1, operand, operand)
                     path.pop()
                 else:
                     path.append("+" + operand_str)
-                    backtrack(i + 1, cur_eval + operand_int, operand_int)
+                    backtrack(i + 1, cur_val + operand, operand)
                     path.pop()
                     path.append("-" + operand_str)
-                    backtrack(i + 1, cur_eval - operand_int, -operand_int)
+                    backtrack(i + 1, cur_val - operand, -operand)
                     path.pop()
                     path.append("*" + operand_str)
-                    new_eval = (cur_eval - last_operand) + (last_operand * operand_int)
-                    backtrack(i + 1, new_eval, last_operand * operand_int)
+                    new_val = (cur_val - last_operand) + (operand * last_operand)
+                    backtrack(i + 1, new_val, (operand * last_operand))
                     path.pop()
         backtrack(0, 0, 0)
         return result
