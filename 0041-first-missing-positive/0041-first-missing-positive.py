5e183aa79b18dec956ec2e78e4a63e1015a42214
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-09 08:07:25 -0500

    Time: 53 ms (33.51%), Space: 29.1 MB (44.82%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -4,15 +4,14 @@ class Solution:
         for i in range(n):
             if nums[i] < 0:
                 nums[i] = 0
-        for i in range(n):
-            val = abs(nums[i])
-            if 1 <= val <= n:
-                if nums[val - 1] > 0:
-                    nums[val - 1] *= -1
-                elif nums[val - 1] == 0:
-                    nums[val - 1] = -math.inf # -1 * (n + 1)
+        for i, num in enumerate(nums):
+            num = abs(num)
+            if 1 <= num <= n:
+                if nums[num - 1] > 0:
+                    nums[num - 1] = -nums[num - 1]
+                elif nums[num - 1] == 0:
+                    nums[num - 1] = -math.inf
         for i in range(1, n + 1):
             if nums[i - 1] >= 0:
                 return i
         return n + 1
-        
