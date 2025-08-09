3308062337b92419b41d777510f2930c4811e5d6
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-09 08:59:36 -0500

    Time: 87 ms (60.26%), Space: 42.9 MB (28.85%) - LeetHub

--- 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.py
+++ 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.py
@@ -1,8 +1,8 @@
 class Solution:
     def sortItems(self, n: int, m: int, group: List[int], beforeItems: List[List[int]]) -> List[int]:
         group_id = m
-        item_graph = []
-        group_graph = []
+        item_graph = collections.defaultdict(list)
+        group_graph = collections.defaultdict(list)
         for i in range(n):
             if group[i] == -1:
                 group[i] = group_id
@@ -10,22 +10,19 @@ class Solution:
         num_groups = group_id
         for i in range(n):
             for before_item in beforeItems[i]:
-                item_graph.append([i, before_item])
+                item_graph[i].append(before_item)
                 if group[i] != group[before_item]:
-                    group_graph.append([group[i], group[before_item]])
+                    group_graph[group[i]].append(group[before_item])
         def topo_sort(num_nodes, prereqs):
             cycle, visited = set(), set()
             output = []
-            adj = collections.defaultdict(list)
-            for node, pre in prereqs:
-                adj[node].append(pre)
             def dfs(node):
                 if node in cycle:
                     return False
                 if node in visited:
                     return True
                 cycle.add(node)
-                for pre in adj[node]:
+                for pre in prereqs[node]:
                     if not dfs(pre):
                         return False
                 cycle.remove(node)
@@ -41,8 +38,7 @@ class Solution:
         if not sorted_items or not sorted_groups:
             return []
         group_map = collections.defaultdict(list)
-        for i in range(n):
-            item = sorted_items[i]
+        for item in sorted_items:
             group_map[group[item]].append(item)
         result = []
         for group_id in sorted_groups:
