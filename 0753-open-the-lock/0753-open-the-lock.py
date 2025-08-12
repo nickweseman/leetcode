f11d8e58fe919c67de79d079b09d4151dd936c0f
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-12 16:37:23 -0500

    Time: 359 ms (74.95%), Space: 19.9 MB (8.39%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.py
+++ 0753-open-the-lock/0753-open-the-lock.py
@@ -1,27 +1,25 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        visited = set(deadends)
-        if "0000" in visited:
+        if "0000" in deadends:
             return -1
-        visited.add("0000")
-        queue = collections.deque()
-        queue.append(("0000", 0)) # lock, turns
+        visited = set(deadends) | set("0000")
+        queue = collections.deque([("0000", 0)]) # lock, turns
         def get_children(lock):
-            result = []
+            children = []
             for i in range(4):
                 digit = int(lock[i])
                 fwd_digit = digit + 1 if digit < 9 else 0
                 bwd_digit = digit - 1 if digit > 0 else 9
-                result.append(lock[:i] + str(fwd_digit) + lock[i+1:])
-                result.append(lock[:i] + str(bwd_digit) + lock[i+1:])
-            return result
+                children.append(lock[:i] + str(fwd_digit) + lock[i+1:])
+                children.append(lock[:i] + str(bwd_digit) + lock[i+1:])
+            return children
         while queue:
-            for _ in range(len(queue)):
-                lock, turns = queue.popleft()
-                if lock == target:
-                    return turns
-                for child in get_children(lock):
-                    if child not in visited:
-                        visited.add(child)
-                        queue.append((child, turns + 1))
+            lock, turns = queue.popleft()
+            if lock == target:
+                return turns
+            for child in get_children(lock):
+                if child not in visited:
+                    visited.add(child)
+                    queue.append((child, turns + 1))
         return -1
+        
