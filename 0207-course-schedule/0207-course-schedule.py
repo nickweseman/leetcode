f035040e6aa154b8a5dd64c579f3613e03e9177c
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-12 18:32:15 -0500

    Time: 4 ms (67.94%), Space: 19.9 MB (10.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,21 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = collections.defaultdict(list)
+        adj = collections.defaultdict(list)
         for course, pre in prerequisites:
-            graph[course].append(pre)
-        cycle = set()
+            adj[course].append(pre)
+        cycle, visited = set(), set()
         def dfs(course):
             if course in cycle:
                 return False
+            if course in visited:
+                return True
             cycle.add(course)
-            for pre in graph[course]:
+            for pre in adj[course]:
                 if not dfs(pre):
                     return False
             cycle.remove(course)
-            graph[course] = []
+            visited.add(course)
             return True
         for course in range(numCourses):
             if not dfs(course):
                 return False
         return True
-            
