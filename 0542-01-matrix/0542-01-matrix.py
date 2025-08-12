863e7b101a5bf8a9b4fff6fae68ca2d9473601b7
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-12 16:28:12 -0500

    Time: 111 ms (86.36%), Space: 20.4 MB (63.96%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,25 +1,21 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         rows, cols = len(mat), len(mat[0])
-        result = [[-1] * cols for _ in range(rows)]
-        visited = set()
         queue = collections.deque()
+        result = [[-1] * cols for _ in range(rows)]
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 0:
                     result[r][c] = 0
                     queue.append((r, c))
-                    visited.add((r, c))
         while queue:
             r, c = queue.popleft()
-            for dr, dc in directions:
+            for dr, dc in dirs:
                 nr, nc = r + dr, c + dc
-                if not (0 <= nr < rows and 0 <= nc < cols) or (nr, nc) in visited:
+                if not (0 <= nr < rows and 0 <= nc < cols) or result[nr][nc] != -1:
                     continue
                 result[nr][nc] = result[r][c] + 1
-                visited.add((nr, nc))
                 queue.append((nr, nc))
         return result
-
-        
+                    
