8715e6ffce3b5fcc4b9f7e142455eb6c9773e98c
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-12 12:33:34 -0500

    Time: 165 ms (51.37%), Space: 19.5 MB (80.65%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,24 +1,20 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        # dp = [[0] * cols for _ in range(rows)] # dp[r][c] -> longest increasing path from (r, c)
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        @functools.lru_cache(None)
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        dp = [[0] * cols for _ in range(rows)]
         def dfs(r, c, prev_val):
             if not (0 <= r < rows and 0 <= c < cols) or matrix[r][c] <= prev_val:
                 return 0
-            # if dp[r][c] != 0:
-            #     return dp[r][c]
-            result = 1
-            for dr, dc in directions:
-                result = max(result, 1 + dfs(r + dr, c + dc, matrix[r][c]))
-            # dp[r][c] = result
-            return result
-        max_val = 0
+            if dp[r][c] != 0:
+                return dp[r][c]
+            best = 1
+            for dr, dc in dirs:
+                best = max(best, 1 + dfs(r + dr, c + dc, matrix[r][c]))
+            dp[r][c] = best
+            return best
+        ans = 0
         for r in range(rows):
             for c in range(cols):
-                max_val = max(max_val, dfs(r, c, -1))
-        return max_val
-            
-
-        
+                ans = max(ans, dfs(r, c, -1))
+        return ans
