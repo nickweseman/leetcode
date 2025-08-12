e851279c09a5535db44fba63dda43ab8f2cb4e14
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-12 17:21:12 -0500

    Time: 87 ms (58.81%), Space: 42.8 MB (26.99%) - LeetHub

--- 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.py
+++ 1309-sort-items-by-groups-respecting-dependencies/1309-sort-items-by-groups-respecting-dependencies.py
@@ -1,21 +1,21 @@
 class Solution:
     def sortItems(self, n: int, m: int, group: List[int], beforeItems: List[List[int]]) -> List[int]:
         group_id = m
-        item_graph = collections.defaultdict(list)
-        group_graph = collections.defaultdict(list)
-        for i in range(n):
-            if group[i] == -1:
-                group[i] = group_id
+        for item in range(n):
+            if group[item] == -1:
+                group[item] = group_id
                 group_id += 1
         num_groups = group_id
-        for i in range(n):
-            for before_item in beforeItems[i]:
-                item_graph[i].append(before_item)
-                if group[i] != group[before_item]:
-                    group_graph[group[i]].append(group[before_item])
+        item_graph = collections.defaultdict(list)
+        group_graph = collections.defaultdict(list)
+        for item in range(n):
+            for before in beforeItems[item]:
+                item_graph[item].append(before)
+                if group[item] != group[before]:
+                    group_graph[group[item]].append(group[before])
         def topo_sort(num_nodes, prereqs):
-            cycle, visited = set(), set()
             output = []
+            cycle, visited = set(), set()
             def dfs(node):
                 if node in cycle:
                     return False
@@ -37,10 +37,10 @@ class Solution:
         sorted_groups = topo_sort(num_groups, group_graph)
         if not sorted_items or not sorted_groups:
             return []
-        group_map = collections.defaultdict(list)
+        group_items = collections.defaultdict(list)
         for item in sorted_items:
-            group_map[group[item]].append(item)
+            group_items[group[item]].append(item)
         result = []
-        for group_id in sorted_groups:
-            result.extend(group_map[group_id])
-        return result         
+        for group in sorted_groups:
+            result.extend(group_items[group])
+        return result
