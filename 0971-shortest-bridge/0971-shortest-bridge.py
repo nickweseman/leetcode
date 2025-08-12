b11c6bf20a92cce0b402f31efe4ca5d696bf0d9c
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-12 18:11:37 -0500

    Time: 100 ms (23.39%), Space: 21.3 MB (14.44%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.py
+++ 0971-shortest-bridge/0971-shortest-bridge.py
@@ -1,19 +1,15 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        n = len(grid)
+        rows, cols = len(grid), len(grid[0])
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         visited = set()
-        def oob(r, c):
-            if not (0 <= r < n and 0 <= c < n) or (r, c) in visited:
-                return True
-            return False
+        def invalid(r, c):
+            return not (0 <= r < rows and 0 <= c < cols) or (r, c) in visited
         def dfs(r, c):
-            if oob(r, c) or grid[r][c] == 0:
+            if invalid(r, c) or grid[r][c] == 0:
                 return
             visited.add((r, c))
-            for dr, dc in directions:
-                if oob(r + dr, c + dc):
-                    continue
+            for dr, dc in dirs:
                 dfs(r + dr, c + dc)
         def bfs():
             queue = collections.deque(visited)
@@ -21,17 +17,17 @@ class Solution:
             while queue:
                 for _ in range(len(queue)):
                     r, c = queue.popleft()
-                    for dr, dc in directions:
+                    for dr, dc in dirs:
                         nr, nc = r + dr, c + dc
-                        if oob(nr, nc):
+                        if invalid(nr, nc):
                             continue
-                        visited.add((nr, nc))
                         if grid[nr][nc] == 1:
                             return turns
                         queue.append((nr, nc))
+                        visited.add((nr, nc))
                 turns += 1
-        for r in range(n):
-            for c in range(n):
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 1:
                     dfs(r, c)
                     return bfs()
