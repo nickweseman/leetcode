adf84c39273ec2dca4e866ff85ced9d5372ab23d
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-15 15:22:58 -0500

    Time: 0 ms (100%), Space: 17.9 MB (75.93%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,14 +1,11 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        for i in range(n-1):
+        for i in range(len(nums) - 1):
             if nums[i] == nums[i + 1]:
                 nums[i] *= 2
                 nums[i + 1] = 0
-        
-        read = write = 0
-
-        while read < n:
+        read, write = 0, 0
+        while read < len(nums):
             if nums[read] != 0:
                 nums[read], nums[write] = nums[write], nums[read]
                 write += 1
