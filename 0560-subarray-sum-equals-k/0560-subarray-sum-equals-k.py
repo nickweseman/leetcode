bb817677d29e517b8eacf4d777fb88b6e3d3ff5f
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-15 10:03:32 -0500

    Time: 47 ms (9.45%), Space: 22.1 MB (5.13%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,11 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix_sums = collections.defaultdict(int)
-        prefix_sums[0] = 1
-        total = 0
+        rolling_sum = 0
         subarrays = 0
+        prefix_count = collections.defaultdict(int)
+        prefix_count[0] = 1
         for num in nums:
-            total += num
-            subarrays += prefix_sums[total - k]
-            prefix_sums[total] += 1
+            rolling_sum += num
+            if prefix_count[rolling_sum - k] > 0:
+                subarrays += prefix_count[rolling_sum - k]
+            prefix_count[rolling_sum] += 1
         return subarrays
