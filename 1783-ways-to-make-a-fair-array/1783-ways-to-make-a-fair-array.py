5c610359c71a1f8bfb540156384988e6dbcb8234
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-15 09:47:08 -0500

    Time: 168 ms (37.82%), Space: 22.8 MB (78.43%) - LeetHub

--- 1783-ways-to-make-a-fair-array/1783-ways-to-make-a-fair-array.py
+++ 1783-ways-to-make-a-fair-array/1783-ways-to-make-a-fair-array.py
@@ -1,18 +1,20 @@
 class Solution:
     def waysToMakeFair(self, nums: List[int]) -> int:
-        left_even, right_even = 0, sum(nums[i] for i in range(0, len(nums), 2))
-        left_odd, right_odd = 0, sum(nums[i] for i in range(1, len(nums), 2))
+        left_even_sum, right_even_sum = 0, sum(num for i, num in enumerate(nums) if i % 2 == 0)
+        left_odd_sum, right_odd_sum = 0, sum(num for i, num in enumerate(nums) if i % 2 == 1)
         num_fair = 0
-
         for i, num in enumerate(nums):
             if i % 2 == 0:
-                right_even -= num
+                right_even_sum -= num
             else:
-                right_odd -= num
-            if left_even + right_odd == left_odd + right_even:
+                right_odd_sum -= num
+            curr_even_sum = left_even_sum + right_odd_sum
+            curr_odd_sum = left_odd_sum + right_even_sum
+            if curr_even_sum == curr_odd_sum:
                 num_fair += 1
             if i % 2 == 0:
-                left_even += num
+                left_even_sum += num
             else:
-                left_odd += num
+                left_odd_sum += num
         return num_fair
+        
