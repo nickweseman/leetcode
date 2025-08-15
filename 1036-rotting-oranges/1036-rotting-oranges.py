209ae220061edb8853e50c26acb5e50de41d34d8
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-15 11:45:51 -0500

    Time: 0 ms (100%), Space: 17.8 MB (44.73%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,25 +1,24 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        num_fresh = 0
-        rotten_queue = collections.deque()
+        fresh = 0
+        queue = collections.deque()
         rows, cols = len(grid), len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        time = 0
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    num_fresh += 1
+                    fresh += 1
                 elif grid[r][c] == 2:
-                    rotten_queue.append((r, c))
-        while rotten_queue and num_fresh > 0:
-            for _ in range(len(rotten_queue)):
-                r, c = rotten_queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if not (0 <= nr < rows and 0 <= nc < cols) or grid[nr][nc] != 1:
-                        continue
-                    grid[nr][nc] = 2
-                    num_fresh -= 1
-                    rotten_queue.append((nr, nc))
-            time += 1
-        return time if num_fresh == 0 else -1
+                    queue.append((r, c, 0))
+        while queue:
+            r, c, turns = queue.popleft()
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if not (0 <= nr < rows and 0 <= nc < cols) or grid[nr][nc] != 1:
+                    continue
+                grid[nr][nc] = 2
+                fresh -= 1
+                if fresh == 0:
+                    return turns + 1
+                queue.append((nr, nc, turns + 1))
+        return 0 if fresh == 0 else -1
