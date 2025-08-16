348481eb7d92e3aa09fc71ffd262d1af79c30dc8
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-16 17:26:59 -0500

    Time: 0 ms (100%), Space: 19.1 MB (41.69%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -6,17 +6,18 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        path = []
-        result = []
-        def dfs(node, sum_so_far):
+        result, path = [], []
+        def backtrack(node, sum_so_far):
             if node:
+                sum_so_far += node.val
                 path.append(node.val)
-                total = sum_so_far + node.val
-                if not node.left and not node.right and total == targetSum:
-                    result.append(path.copy())
-                else:
-                    dfs(node.left, total)
-                    dfs(node.right, total)
+                if not node.left and not node.right:
+                    if targetSum == sum_so_far:
+                        result.append(path.copy())
+                    path.pop()
+                    return
+                backtrack(node.left, sum_so_far)
+                backtrack(node.right, sum_so_far)
                 path.pop()
-        dfs(root, 0)
+        backtrack(root, 0)
         return result
