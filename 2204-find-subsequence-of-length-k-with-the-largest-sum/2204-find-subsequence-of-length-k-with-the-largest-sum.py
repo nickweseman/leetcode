48307262235565091ac7cdddbf629d23babe4d6c
Nick Weseman <trumpernickel2@yahoo.com>
2025-08-17 09:19:18 -0500

    Time: 4 ms (42.51%), Space: 18.3 MB (15.05%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -3,8 +3,8 @@ class Solution:
         min_heap = []
         for i, num in enumerate(nums):
             if len(min_heap) < k:
-                heapq.heappush(min_heap, ((num, i)))
+                heapq.heappush(min_heap, (num, i))
             else:
-                heapq.heappushpop(min_heap, ((num, i)))
-        min_heap.sort(key = lambda x: x[1])
-        return [num for num, _ in min_heap]
+                heapq.heappushpop(min_heap, (num, i))
+        min_heap.sort(key = lambda x : x[1])
+        return [num for num, i in min_heap]
